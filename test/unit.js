8af18fa47e67a5fef4f16eaecfb5ade14b1ea2db
Luke Childs <lukechilds123@gmail.com>
2017-07-07 15:16:10 +0100

    Test Keyv cannot be invoked without \'new\'

--- test/unit.js
+++ test/unit.js
@@ -4,3 +4,8 @@ import Keyv from '../';
 test('Keyv is a function', t => {
 	t.is(typeof Keyv, 'function');
 });
+
+test('Keyv cannot be invoked without \'new\'', t => {
+	t.throws(() => Keyv()); // eslint-disable-line new-cap
+	t.notThrows(() => new Keyv());
+});
