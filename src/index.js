178c0ef7222b297df2e798e87761f64a3825b4b1
Luke Childs <lukechilds123@gmail.com>
2017-07-07 15:11:44 +0100

    Wrap Map API

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 'use strict';
 
-class keyv {}
+class keyv {
+	constructor() {
+		this.store = new Map();
+	}
+
+	get(key) {
+		return this.store.get(key);
+	}
+
+	set(key, value) {
+		return this.store.set(key, value);
+	}
+
+	delete(key) {
+		return this.store.delete(key);
+	}
+}
 
 module.exports = keyv;
