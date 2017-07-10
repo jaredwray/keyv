b8999c96dfc35a71923770b7c88d1defa2001c63
Luke Childs <lukechilds123@gmail.com>
2017-07-10 17:51:31 +0100

    Save all opts in this.opts

--- src/index.js
+++ src/index.js
@@ -2,37 +2,37 @@
 
 class Keyv {
 	constructor(opts) {
-		opts = opts || {};
-		this.store = opts.store || new Map();
+		this.opts = opts || {};
+		this.opts.store = this.opts.store || new Map();
 	}
 
 	get(key) {
-		return Promise.resolve(this.store.get(key)).then(data => {
+		return Promise.resolve(this.opts.store.get(key)).then(data => {
 			if (!data) {
 				return undefined;
 			}
-			if (!this.store.ttlSupport && Date.now() > data.expires) {
+			if (!this.opts.store.ttlSupport && Date.now() > data.expires) {
 				this.delete(key);
 				return undefined;
 			}
-			return this.store.ttlSupport ? data : data.value;
+			return this.opts.store.ttlSupport ? data : data.value;
 		});
 	}
 
 	set(key, value, ttl) {
 		let set;
-		if (this.store.ttlSupport) {
-			set = this.store.set(key, value, ttl);
+		if (this.opts.store.ttlSupport) {
+			set = this.opts.store.set(key, value, ttl);
 		} else {
 			const expires = (typeof ttl === 'number') ? (Date.now() + ttl) : undefined;
 			const data = { value, expires };
-			set = this.store.set(key, data);
+			set = this.opts.store.set(key, data);
 		}
 		return Promise.resolve(set).then(() => value);
 	}
 
 	delete(key) {
-		return Promise.resolve(this.store.delete(key));
+		return Promise.resolve(this.opts.store.delete(key));
 	}
 }
 
