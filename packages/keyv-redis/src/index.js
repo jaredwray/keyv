055514edf233264a19fd38ac0859e8d1c9afbc2b
Luke Childs <lukechilds123@gmail.com>
2017-07-11 16:28:09 +0100

    Adapt Redis API

--- packages/keyv-redis/src/index.js
+++ packages/keyv-redis/src/index.js
@@ -1,16 +1,29 @@
 'use strict';
 
+const redis = require('redis');
+const pify = require('pify');
+
 class KeyvRedis {
 	constructor(opts) {
+		this.client = redis.createClient(opts);
 	}
 
 	get(key) {
+		return pify(this.client.get.bind(this.client))(key)
+			.then(JSON.parse);
 	}
 
 	set(key, value) {
+		return Promise.resolve()
+			.then(() => {
+				value = JSON.stringify(value);
+				return pify(this.client.set.bind(this.client))(key, value);
+			});
 	}
 
 	delete(key) {
+		return pify(this.client.del.bind(this.client))(key)
+			.then(items => items > 0);
 	}
 }
 
