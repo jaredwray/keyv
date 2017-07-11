439cf2bbb1cbda4c3dfebc125de43b7c5f762e7f
Luke Childs <lukechilds123@gmail.com>
2017-07-11 14:42:01 +0100

    Get shorter reference to store

--- src/index.js
+++ src/index.js
@@ -7,33 +7,36 @@ class Keyv {
 	}
 
 	get(key) {
-		return Promise.resolve(this.opts.store.get(key)).then(data => {
+		const store = this.opts.store;
+		return Promise.resolve(store.get(key)).then(data => {
 			if (!data) {
 				return undefined;
 			}
-			if (!this.opts.store.ttlSupport && Date.now() > data.expires) {
+			if (!store.ttlSupport && Date.now() > data.expires) {
 				this.delete(key);
 				return undefined;
 			}
-			return this.opts.store.ttlSupport ? data : data.value;
+			return store.ttlSupport ? data : data.value;
 		});
 	}
 
 	set(key, value, ttl) {
 		ttl = ttl || this.opts.ttl;
+		const store = this.opts.store;
 		let set;
-		if (this.opts.store.ttlSupport) {
-			set = this.opts.store.set(key, value, ttl);
+		if (store.ttlSupport) {
+			set = store.set(key, value, ttl);
 		} else {
 			const expires = (typeof ttl === 'number') ? (Date.now() + ttl) : undefined;
 			const data = { value, expires };
-			set = this.opts.store.set(key, data);
+			set = store.set(key, data);
 		}
 		return Promise.resolve(set).then(() => value);
 	}
 
 	delete(key) {
-		return Promise.resolve(this.opts.store.delete(key));
+		const store = this.opts.store;
+		return Promise.resolve(store.delete(key));
 	}
 }
 
