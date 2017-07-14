9b951ea6e1216aeecafb3050355dc207c119a3c1
Luke Childs <lukechilds123@gmail.com>
2017-07-14 16:59:45 +0100

    Add set method

--- packages/keyv-mongo/src/index.js
+++ packages/keyv-mongo/src/index.js
@@ -9,16 +9,24 @@ class KeyvMongo {
 		if (typeof opts === 'string') {
 			opts = { url: opts };
 		}
-		this.opts = Object.assign({
+		opts = Object.assign({
 			url: 'mongodb://127.0.0.1:27017',
 			collection: 'keyv'
 		}, opts);
-		this.db = mongojs(this.opts.url);
+		this.db = mongojs(opts.url);
+
+		const collection = this.db.collection(opts.collection);
+		this.mongo = ['update'].reduce((obj, method) => {
+			obj[method] = pify(collection[method].bind(collection));
+			return obj;
+		}, {});
 	}
 
 	get(key) {}
 
-	set(key, value) {}
+	set(key, value) {
+		return this.mongo.update({ key }, { key, value }, { upsert: true });
+	}
 
 	delete(key) {}
 
