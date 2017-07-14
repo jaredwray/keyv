16ba3c8cb5e1e6bb02504c0a29205c8ae697874d
Luke Childs <lukechilds123@gmail.com>
2017-07-14 16:23:43 +0100

    Connect to MongoDB on init

--- packages/keyv-mongo/src/index.js
+++ packages/keyv-mongo/src/index.js
@@ -1,10 +1,26 @@
 'use strict';
 
+const MongoClient = require('mongodb').MongoClient;
 const pify = require('pify');
 
 class KeyvMongo {
 	constructor(opts) {
 		this.ttlSupport = false;
+		if (typeof opts === 'string') {
+			opts = { url: opts };
+		}
+		opts = Object.assign({
+			url: 'mongodb://127.0.0.1:27017',
+			collection: 'keyv'
+		}, opts);
+		this.connected = new Promise((resolve, reject) => {
+			MongoClient.connect(opts.url, (err, db) => {
+				if(err) {
+					return reject(err);
+				}
+				return resolve(db.collection(opts.collection));
+			});
+		});
 	}
 
 	get(key) {}
