775af8dfc67cb7541b65f7aff0c57ae7adbed677
Luke Childs <lukechilds123@gmail.com>
2017-07-14 20:01:42 +0100

    Test Redis URL can be passed in as string

--- packages/keyv-mongo/test/test.js
+++ packages/keyv-mongo/test/test.js
@@ -5,3 +5,8 @@ import KeyvMongo from '../';
 
 const store = new KeyvMongo();
 keyvApiTests(test, Keyv, store);
+
+test('Redis URL can be passed in as string', t => {
+	const store = new KeyvMongo('foo');
+	t.is(store.opts.url, 'foo');
+});
