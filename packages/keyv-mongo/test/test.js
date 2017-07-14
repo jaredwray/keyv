27f399c359cb347abdbed45af7b109e4071a99e1
Luke Childs <lukechilds123@gmail.com>
2017-07-14 20:02:18 +0100

    Test collection option merges into default options

--- packages/keyv-mongo/test/test.js
+++ packages/keyv-mongo/test/test.js
@@ -10,3 +10,11 @@ test('Redis URL can be passed in as string', t => {
 	const store = new KeyvMongo('foo');
 	t.is(store.opts.url, 'foo');
 });
+
+test('Collection option merges into default options', t => {
+	const store = new KeyvMongo({ collection: 'foo' });
+	t.deepEqual(store.opts, {
+		url: 'mongodb://127.0.0.1:27017',
+		collection: 'foo'
+	});
+});
