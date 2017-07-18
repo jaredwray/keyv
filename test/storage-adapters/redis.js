1eb5b293af6a5afc0283efb3eabcf42d560ad264
Luke Childs <lukechilds123@gmail.com>
2017-07-18 12:31:08 +0100

    Test redis connection errors are emitted

--- test/storage-adapters/redis.js
+++ test/storage-adapters/redis.js
@@ -6,3 +6,11 @@ test('connection string automatically requires module', async t => {
 	await keyv.set('foo', 'bar');
 	t.is(await keyv.get('foo'), 'bar');
 });
+
+test.cb('connection errors are emitted', t => {
+	const keyv = new Keyv('redis://foo');
+	keyv.on('error', () => {
+		t.pass();
+		t.end();
+	});
+});
