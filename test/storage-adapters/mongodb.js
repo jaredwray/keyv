01a88f2f552a7c9b1f33dc508514b337d33605a4
Luke Childs <lukechilds123@gmail.com>
2017-07-25 16:56:50 +0100

    Copy official tests into keyv-test-suite

--- test/storage-adapters/mongodb.js
+++ test/storage-adapters/mongodb.js
@@ -1,20 +1,8 @@
 import test from 'ava';
-import keyvTestSuite from 'keyv-test-suite';
+import keyvTestSuite, { keyvOfficialTests } from 'keyv-test-suite';
 import Keyv from 'get-root-module';
 
-test('connection string automatically requires module', async t => {
-	const keyv = new Keyv('mongodb://127.0.0.1:27017');
-	await keyv.set('foo', 'bar');
-	t.is(await keyv.get('foo'), 'bar');
-});
-
-test.cb('connection errors are emitted', t => {
-	const keyv = new Keyv('mongodb://127.0.0.1:1234');
-	keyv.on('error', () => {
-		t.pass();
-		t.end();
-	});
-});
+keyvOfficialTests(test, Keyv, 'mongodb://127.0.0.1:27017', 'mongodb://127.0.0.1:1234');
 
 const store = new (require('keyv-mongo'))('mongodb://127.0.0.1:27017'); // eslint-disable-line import/newline-after-import
 keyvTestSuite(test, Keyv, store);
--- test/storage-adapters/redis.js
+++ test/storage-adapters/redis.js
@@ -1,20 +1,8 @@
 import test from 'ava';
-import keyvTestSuite from 'keyv-test-suite';
+import keyvTestSuite, { keyvOfficialTests } from 'keyv-test-suite';
 import Keyv from 'get-root-module';
 
-test('connection string automatically requires module', async t => {
-	const keyv = new Keyv('redis://localhost');
-	await keyv.set('foo', 'bar');
-	t.is(await keyv.get('foo'), 'bar');
-});
-
-test.cb('connection errors are emitted', t => {
-	const keyv = new Keyv('redis://foo');
-	keyv.on('error', () => {
-		t.pass();
-		t.end();
-	});
-});
+keyvOfficialTests(test, Keyv, 'redis://localhost', 'redis://foo');
 
 const store = new (require('keyv-redis'))('redis://localhost'); // eslint-disable-line import/newline-after-import
 keyvTestSuite(test, Keyv, store);
