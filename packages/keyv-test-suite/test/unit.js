e7a17670972ccbdbaf0b9b4b0f230fa2a68f9b18
Luke Childs <lukechilds123@gmail.com>
2017-07-25 17:48:47 +0100

    Test keyvOfficialTests against keyv-redis

--- packages/keyv-test-suite/test/unit.js
+++ packages/keyv-test-suite/test/unit.js
@@ -1,6 +1,8 @@
 import test from 'ava';
 import Keyv from 'keyv';
-import keyvTestSuite from 'get-root-module';
+import keyvTestSuite, { keyvOfficialTests } from 'get-root-module';
+
+keyvOfficialTests(test, Keyv, 'redis://localhost', 'redis://foo');
 
 const store = new Map();
 keyvTestSuite(test, Keyv, store);
