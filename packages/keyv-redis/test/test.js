99472166b611006ce6724f3571003844c33b89eb
Luke Childs <lukechilds123@gmail.com>
2017-07-28 16:58:58 +0100

    Use "this"

--- packages/keyv-redis/test/test.js
+++ packages/keyv-redis/test/test.js
@@ -1,7 +1,7 @@
 import test from 'ava';
 import keyvTestSuite, { keyvOfficialTests } from 'keyv-test-suite';
 import Keyv from 'keyv';
-import KeyvRedis from '../';
+import KeyvRedis from 'this';
 
 keyvOfficialTests(test, Keyv, 'redis://localhost', 'redis://foo');
 
