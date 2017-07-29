418e03d23cea0099e7ef81cc4ef90d16d73ce9c2
Luke Childs <lukechilds123@gmail.com>
2017-07-29 14:09:57 +0100

    Make invalid connection string more clear

--- packages/keyv-sql/test/test.js
+++ packages/keyv-sql/test/test.js
@@ -3,7 +3,7 @@ import keyvTestSuite, { keyvOfficialTests } from 'keyv-test-suite';
 import Keyv from 'keyv';
 import KeyvSQLite from 'this';
 
-keyvOfficialTests(test, Keyv, 'sqlite://:memory:', 'sqlite://path/to/database.sqlite');
+keyvOfficialTests(test, Keyv, 'sqlite://:memory:', 'sqlite://non/existent/database.sqlite');
 
 const store = () => new KeyvSQLite();
 keyvTestSuite(test, Keyv, store);
