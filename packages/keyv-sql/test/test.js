4a51ea06e63ff133a968efc3315ac1be04f59a8e
Luke Childs <lukechilds123@gmail.com>
2017-07-29 14:34:12 +0100

    Don't run tests in memory

--- packages/keyv-sql/test/test.js
+++ packages/keyv-sql/test/test.js
@@ -3,7 +3,7 @@ import keyvTestSuite, { keyvOfficialTests } from 'keyv-test-suite';
 import Keyv from 'keyv';
 import KeyvSqlite from 'this';
 
-keyvOfficialTests(test, Keyv, 'sqlite://:memory:', 'sqlite://non/existent/database.sqlite');
+keyvOfficialTests(test, Keyv, 'sqlite://test/testdb.sqlite', 'sqlite://non/existent/database.sqlite');
 
-const store = () => new KeyvSqlite('sqlite://:memory:');
+const store = () => new KeyvSqlite('sqlite://test/testdb.sqlite');
 keyvTestSuite(test, Keyv, store);
