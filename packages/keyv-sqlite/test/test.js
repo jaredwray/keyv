7a35ed8a658a79f9c6964236d328564543a92028
Luke Childs <lukechilds123@gmail.com>
2017-07-29 14:14:42 +0100

    KeyvSQLite => KeyvSqlite

--- packages/keyv-sqlite/test/test.js
+++ packages/keyv-sqlite/test/test.js
@@ -1,9 +1,9 @@
 import test from 'ava';
 import keyvTestSuite, { keyvOfficialTests } from 'keyv-test-suite';
 import Keyv from 'keyv';
-import KeyvSQLite from 'this';
+import KeyvSqlite from 'this';
 
 keyvOfficialTests(test, Keyv, 'sqlite://:memory:', 'sqlite://non/existent/database.sqlite');
 
-const store = () => new KeyvSQLite('sqlite://:memory:');
+const store = () => new KeyvSqlite('sqlite://:memory:');
 keyvTestSuite(test, Keyv, store);
