5e38de695e5fc0331f738bfe819d856cce9673a2
Luke Childs <lukechilds123@gmail.com>
2017-07-30 00:17:04 +0100

    Keyvpostgres => KeyvPostgres

--- packages/keyv-postgres/test/test.js
+++ packages/keyv-postgres/test/test.js
@@ -1,9 +1,9 @@
 import test from 'ava';
 import keyvTestSuite, { keyvOfficialTests } from 'keyv-test-suite';
 import Keyv from 'keyv';
-import Keyvpostgres from 'this';
+import KeyvPostgres from 'this';
 
 keyvOfficialTests(test, Keyv, 'postgresql://postgres@localhost:5432/keyv_test', 'postgresql://foo');
 
-const store = () => new Keyvpostgres('postgresql://postgres@localhost:5432/keyv_test');
+const store = () => new KeyvPostgres('postgresql://postgres@localhost:5432/keyv_test');
 keyvTestSuite(test, Keyv, store);
