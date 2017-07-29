d9fb80d5e615c8c3bf9feafb2667393b933c7bc4
Luke Childs <lukechilds123@gmail.com>
2017-07-29 18:22:48 +0100

    Test against SQLite

--- packages/keyv-sql/test/test.js
+++ packages/keyv-sql/test/test.js
@@ -1,16 +1,14 @@
 import test from 'ava';
-import keyvTestSuite, { keyvOfficialTests } from 'keyv-test-suite';
+import keyvTestSuite from 'keyv-test-suite';
 import Keyv from 'keyv';
-import KeyvSqlite from 'this';
+import KeyvSequelize from 'this';
 
-keyvOfficialTests(test, Keyv, 'sqlite://test/testdb.sqlite', 'sqlite://non/existent/database.sqlite');
+const sqliteOpts = {
+	dialect: 'sqlite',
+	uri: 'sqlite://test/testdb.sqlite',
+	table: 'keyv',
+	logging: false
+};
 
-const store = () => new KeyvSqlite('sqlite://test/testdb.sqlite');
+const store = () => new KeyvSequelize(sqliteOpts);
 keyvTestSuite(test, Keyv, store);
-
-test('options have expected defaults', t => {
-	const store = new KeyvSqlite();
-	t.is(store.opts.uri, 'sqlite://:memory:');
-	t.is(store.opts.table, 'keyv');
-	t.false(store.opts.logging);
-});
