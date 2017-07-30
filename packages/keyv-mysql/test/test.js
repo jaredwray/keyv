e228aff6c56174149ada4edeb8d6d09ca94348a8
Luke Childs <lukechilds123@gmail.com>
2017-07-30 08:03:31 +0100

    Fix Travis MySQL

--- packages/keyv-mysql/test/test.js
+++ packages/keyv-mysql/test/test.js
@@ -3,7 +3,7 @@ import keyvTestSuite, { keyvOfficialTests } from 'keyv-test-suite';
 import Keyv from 'keyv';
 import KeyvMysql from 'this';
 
-keyvOfficialTests(test, Keyv, 'mysql://localhost/keyv_test', 'mysqlql://foo');
+keyvOfficialTests(test, Keyv, 'mysql://mysql@localhost/keyv_test', 'mysqlql://foo');
 
-const store = () => new KeyvMysql('mysql://localhost/keyv_test');
+const store = () => new KeyvMysql('mysql://mysql@localhost/keyv_test');
 keyvTestSuite(test, Keyv, store);
