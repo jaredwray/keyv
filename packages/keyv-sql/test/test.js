bb530cfbd45266e4e754c6359fcb684f3ddb863c
Luke Childs <lukechilds123@gmail.com>
2017-07-30 14:20:36 +0100

    Extract keyv-sql from keyv-sequelize

--- packages/keyv-sql/test/test.js
+++ packages/keyv-sql/test/test.js
@@ -1,18 +1,18 @@
 import test from 'ava';
 import keyvTestSuite from 'keyv-test-suite';
 import Keyv from 'keyv';
-import KeyvSequelize from 'this';
+import KeyvSQL from 'this';
 
 const sqliteOpts = {
 	dialect: 'sqlite',
 	uri: 'sqlite://test/testdb.sqlite'
 };
 
-const store = () => new KeyvSequelize(sqliteOpts);
+const store = () => new KeyvSQL(sqliteOpts);
 keyvTestSuite(test, Keyv, store);
 
 test.serial.cb('connection errors are emitted', t => {
-	const store = new KeyvSequelize({ uri: 'sqlite://non/existent/database.sqlite' });
+	const store = new KeyvSQL({ uri: 'sqlite://non/existent/database.sqlite' });
 	const keyv = new Keyv({ store });
 	keyv.on('error', () => {
 		t.pass();
