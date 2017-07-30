613f432e81a66bb5d82c39c4e434ed8e40ae464f
Luke Childs <lukechilds123@gmail.com>
2017-07-30 08:39:14 +0100

    Test against keyv-mysql

---
+++ test/storage-adapters/mysql.js
@@ -0,0 +1,9 @@
+import test from 'ava';
+import keyvTestSuite, { keyvOfficialTests } from 'keyv-test-suite';
+import Keyv from 'this';
+import KeyvMysql from 'keyv-mysql';
+
+keyvOfficialTests(test, Keyv, 'mysql://mysql@localhost/keyv_test', 'mysql://foo');
+
+const store = () => new KeyvMysql('mysql://mysql@localhost/keyv_test');
+keyvTestSuite(test, Keyv, store);
