75fea6e60ac5677f33c636abd5b2bc9b75a8d584
Luke Childs <lukechilds123@gmail.com>
2017-07-31 14:58:40 +0100

    Use our fork of sql until REPLACE support is merged
    https://github.com/brianc/node-sql/pull/368

--- packages/keyv-sql/src/index.js
+++ packages/keyv-sql/src/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const EventEmitter = require('events');
-const Sql = require('sql').Sql;
+const Sql = require('sql-replace-fork').Sql;
 
 class KeyvSql extends EventEmitter {
 	constructor(opts) {
