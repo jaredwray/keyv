5f9977cc22b46aedfe477c80288040d63a0d2068
Luke Childs <lukechilds123@gmail.com>
2017-08-03 10:19:43 +0100

    Test against SQLite not Redis

--- packages/keyv-test-suite/test/unit.js
+++ packages/keyv-test-suite/test/unit.js
@@ -2,7 +2,7 @@ import test from 'ava';
 import Keyv from 'keyv';
 import keyvTestSuite, { keyvOfficialTests } from 'this';
 
-keyvOfficialTests(test, Keyv, 'redis://localhost', 'redis://foo');
+keyvOfficialTests(test, Keyv, 'sqlite://test/testdb.sqlite', 'sqlite://non/existent/database.sqlite');
 
 const store = () => new Map();
 keyvTestSuite(test, Keyv, store);
