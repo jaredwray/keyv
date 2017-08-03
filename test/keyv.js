1ec4ffa5431bfe3b5968ce35ae6b46de8bed5c9b
Luke Childs <lukechilds123@gmail.com>
2017-08-03 10:50:50 +0100

    Mock dates in tests

--- test/keyv.js
+++ test/keyv.js
@@ -1,15 +1,15 @@
 import test from 'ava';
-import delay from 'delay';
+import tk from 'timekeeper';
 import keyvTestSuite from 'keyv-test-suite';
 import Keyv from 'this';
 
-test('Keyv is a class', t => {
+test.serial('Keyv is a class', t => {
 	t.is(typeof Keyv, 'function');
 	t.throws(() => Keyv()); // eslint-disable-line new-cap
 	t.notThrows(() => new Keyv());
 });
 
-test('Keyv accepts storage adapters', async t => {
+test.serial('Keyv accepts storage adapters', async t => {
 	const store = new Map();
 	const keyv = new Keyv({ store });
 	t.is(store.size, 0);
@@ -18,7 +18,7 @@ test('Keyv accepts storage adapters', async t => {
 	t.is(store.size, 1);
 });
 
-test('Keyv hands tll functionality over to ttl supporting stores', async t => {
+test.serial('Keyv hands tll functionality over to ttl supporting stores', async t => {
 	t.plan(3);
 	const store = new Map();
 	store.ttlSupport = true;
@@ -30,20 +30,24 @@ test('Keyv hands tll functionality over to ttl supporting stores', async t => {
 	const keyv = new Keyv({ store });
 	await keyv.set('foo', 'bar', 100);
 	t.is(await keyv.get('foo'), 'bar');
-	await delay(150);
+	tk.freeze(Date.now() + 150);
 	t.is(await keyv.get('foo'), 'bar');
+	tk.reset();
 });
 
-test('Keyv respects default tll option', async t => {
+test.serial('Keyv respects default tll option', async t => {
 	const store = new Map();
 	const keyv = new Keyv({ store, ttl: 100 });
 	await keyv.set('foo', 'bar');
 	t.is(await keyv.get('foo'), 'bar');
-	await delay(150);
+	tk.freeze(Date.now() + 150);
 	t.is(await keyv.get('foo'), undefined);
+	tk.reset();
 });
 
-test('.set(key, val, ttl) overwrites default tll option', async t => {
+test.serial('.set(key, val, ttl) overwrites default tll option', async t => {
+	const startTime = Date.now();
+	tk.freeze(startTime);
 	const store = new Map();
 	const keyv = new Keyv({ store, ttl: 200 });
 	await keyv.set('foo', 'bar');
@@ -52,15 +56,16 @@ test('.set(key, val, ttl) overwrites default tll option', async t => {
 	t.is(await keyv.get('foo'), 'bar');
 	t.is(await keyv.get('fizz'), 'buzz');
 	t.is(await keyv.get('ping'), 'pong');
-	await delay(150);
+	tk.freeze(startTime + 150);
 	t.is(await keyv.get('foo'), 'bar');
 	t.is(await keyv.get('fizz'), undefined);
 	t.is(await keyv.get('ping'), 'pong');
-	await delay(100);
+	tk.freeze(startTime + 250);
 	t.is(await keyv.get('foo'), undefined);
 	t.is(await keyv.get('ping'), 'pong');
-	await delay(100);
+	tk.freeze(startTime + 350);
 	t.is(await keyv.get('ping'), undefined);
+	tk.reset();
 });
 
 const store = () => new Map();
