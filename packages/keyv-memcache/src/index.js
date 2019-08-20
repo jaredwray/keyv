54d9be0ce202bc89f23783d7e61e62e82a29e1b7
Jared Wray <me@jaredwray.com>
2019-08-20 13:41:17 -0700

    added emits

--- packages/keyv-memcache/src/index.js
+++ packages/keyv-memcache/src/index.js
@@ -20,6 +20,7 @@ class KeyvMemcache extends EventEmitter {
 		return new Promise((resolve, reject) => {
 		this.client.get(key, (err, value, flags) => {
 			if (err) {
+				this.emit('error', err);
 				reject(err);
 			} else {
 				resolve(value, flags);
@@ -32,6 +33,7 @@ class KeyvMemcache extends EventEmitter {
 		return new Promise((resolve, reject) => {
 		this.client.set(key, value, { ttl }, (err, success) => {
 			if (err) {
+				this.emit('error', err);
 				reject(err);
 			} else {
 				resolve(success);
@@ -44,6 +46,7 @@ class KeyvMemcache extends EventEmitter {
 		return new Promise((resolve, reject) => {
 		this.client.delete(key, (err, success) => {
 			if (err) {
+				this.emit('error', err);
 				reject(err);
 			} else {
 				resolve(success);
@@ -56,6 +59,7 @@ class KeyvMemcache extends EventEmitter {
 		return new Promise((resolve, reject) => {
 		this.client.flush((err, success) => {
 			if (err) {
+				this.emit('error', err);
 				reject(err);
 			} else {
 				resolve(success);
