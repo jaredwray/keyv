49203a4945d93542a56df6a9cde388ca46a2e1e1
Jared Wray <me@jaredwray.com>
2019-08-20 13:54:28 -0700

    Update test.js

--- packages/keyv-memcache/test/test.js
+++ packages/keyv-memcache/test/test.js
@@ -3,7 +3,7 @@ import keyvTestSuite, { keyvOfficialTests } from '@keyv/test-suite';
 import Keyv from 'keyv';
 import KeyvMemcache from '../src';
 
-keyvOfficialTests(test, Keyv, 'localhost:11211', 'localhost:4343');
+keyvOfficialTests(test, Keyv, 'memcache://localhost:11211', 'memcache://foo');
 
 const store = () => new KeyvMemcache();
 keyvTestSuite(test, Keyv, store);
