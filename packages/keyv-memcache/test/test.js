4c192064094f6dffb7face00f19ffeb1a4d417eb
Jared Wray <me@jaredwray.com>
2019-08-20 13:41:39 -0700

    getting tests to just work locally
    memcached server running locally to have it function

--- packages/keyv-memcache/test/test.js
+++ packages/keyv-memcache/test/test.js
@@ -3,7 +3,7 @@ import keyvTestSuite, { keyvOfficialTests } from '@keyv/test-suite';
 import Keyv from 'keyv';
 import KeyvMemcache from '../src';
 
-keyvOfficialTests(test, Keyv, '', '');
+keyvOfficialTests(test, Keyv, 'localhost:11211', 'localhost:4343');
 
 const store = () => new KeyvMemcache();
 keyvTestSuite(test, Keyv, store);
