4d4e24c977c2af1dd19a3afc917378d1cb2601ca
Jared Wray <me@jaredwray.com>
2021-10-05 17:17:18 -0700

    same user and pass

--- packages/keyv-postgres/test/test.js
+++ packages/keyv-postgres/test/test.js
@@ -3,7 +3,7 @@ import keyvTestSuite, { keyvOfficialTests } from '@keyv/test-suite';
 import Keyv from 'keyv';
 import KeyvPostgres from 'this';
 
-keyvOfficialTests(test, Keyv, 'postgresql://postgres@localhost:5432/keyv_test', 'postgresql://foo');
+keyvOfficialTests(test, Keyv, 'postgresql://postgres:postgres@localhost:5432/keyv_test', 'postgresql://foo');
 
-const store = () => new KeyvPostgres({ uri: 'postgresql://postgres@localhost:5432/keyv_test' });
+const store = () => new KeyvPostgres({ uri: 'postgresql://postgres:postgres@localhost:5432/keyv_test' });
 keyvTestSuite(test, Keyv, store);
