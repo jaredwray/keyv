3d62aeb08360c0c7def4786c4768db189fbd0246
Jared Wray <me@jaredwray.com>
2021-10-12 09:23:49 -0700

    Update test.js

--- packages/keyv-postgres/test/test.js
+++ packages/keyv-postgres/test/test.js
@@ -1,8 +1,6 @@
 const test = require('ava');
-const keyvTestSuite = require('@keyv/test-suite');
 const { keyvOfficialTests } = require('@keyv/test-suite');
 const Keyv = require('keyv');
-const KeyvPostgres = require('this');
 
 keyvOfficialTests(test, Keyv, 'postgresql://postgres:postgres@localhost:5432/keyv_test', 'postgresql://foo');
 
