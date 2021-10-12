ae060f080fca479c5c6cb7cb869f90a11db97f79
Jared Wray <me@jaredwray.com>
2021-10-12 09:26:09 -0700

    adding in api tests

--- packages/keyv-postgres/test/test.js
+++ packages/keyv-postgres/test/test.js
@@ -1,6 +1,10 @@
 const test = require('ava');
 const { keyvOfficialTests } = require('@keyv/test-suite');
 const Keyv = require('keyv');
+const KeyvPostgres = require('this');
+const keyvTestSuite = require('@keyv/test-suite');
 
 keyvOfficialTests(test, Keyv, 'postgresql://postgres:postgres@localhost:5432/keyv_test', 'postgresql://foo');
 
+const store = () => new KeyvPostgres({ uri: 'postgresql://postgres:postgres@localhost:5432/keyv_test' });
+keyvTestSuite.keyvApiTests(test, Keyv, store);
