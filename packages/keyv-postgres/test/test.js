e11c32f3809053b3f9883c2c95c7a867705534ee
Jared Wray <me@jaredwray.com>
2021-10-13 10:42:12 -0700

    updated tests to more uniform

--- packages/keyv-postgres/test/test.js
+++ packages/keyv-postgres/test/test.js
@@ -1,12 +1,11 @@
 const test = require('ava');
+const keyvTestSuite = require('@keyv/test-suite').default;
 const { keyvOfficialTests } = require('@keyv/test-suite');
 const Keyv = require('keyv');
 const KeyvPostgres = require('this');
-const keyvTestSuite = require('@keyv/test-suite');
 
 keyvOfficialTests(test, Keyv, 'postgresql://postgres:postgres@localhost:5432/keyv_test', 'postgresql://foo');
 
-const storeApi = () => new KeyvPostgres({ uri: 'postgresql://postgres:postgres@localhost:5432/keyv_test', table: 'cacheApi' });
-const storeValue = () => new KeyvPostgres({ uri: 'postgresql://postgres:postgres@localhost:5432/keyv_test', table: 'cacheValue' });
-keyvTestSuite.keyvApiTests(test, Keyv, storeApi);
-keyvTestSuite.keyvValueTests(test, Keyv, storeValue);
+const store = () => new KeyvPostgres({ uri: 'postgresql://postgres:postgres@localhost:5432/keyv_test' });
+keyvTestSuite(test, Keyv, store);
+
