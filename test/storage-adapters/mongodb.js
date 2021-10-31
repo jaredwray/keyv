4332fe554337dae587bebd34d406aae82d6c9d01
Jared Wray <me@jaredwray.com>
2021-10-31 15:06:12 -0700

    removing keyvOfficialTests for validation

--- test/storage-adapters/mongodb.js
+++ test/storage-adapters/mongodb.js
@@ -1,10 +1,7 @@
 const test = require('ava');
 const keyvTestSuite = require('@keyv/test-suite').default;
-const { keyvOfficialTests } = require('@keyv/test-suite');
 const Keyv = require('this');
 const KeyvMongo = require('@keyv/mongo');
-
-keyvOfficialTests(test, Keyv, 'mongodb://127.0.0.1:27017', 'mongodb://127.0.0.1:1234');
 
 const store = () => new KeyvMongo('mongodb://127.0.0.1:27017');
 keyvTestSuite(test, Keyv, store);
