1b654a37da4a8be9dc583bcda52329f60172a6da
Jared Wray <me@jaredwray.com>
2021-11-22 13:41:29 -0800

    adding in keyv official tests

--- packages/mysql/test/test.js
+++ packages/mysql/test/test.js
@@ -1,7 +1,10 @@
 const test = require('ava');
 const keyvTestSuite = require('@keyv/test-suite').default;
+const { keyvOfficialTests } = require('@keyv/test-suite');
 const Keyv = require('keyv');
 const KeyvMysql = require('this');
+
+keyvOfficialTests(test, Keyv, 'mysql://root@localhost/keyv_test', 'mysql://foo');
 
 const store = () => new KeyvMysql('mysql://root@localhost/keyv_test');
 keyvTestSuite(test, Keyv, store);
