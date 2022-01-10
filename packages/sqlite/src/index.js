fd08a95a73f9200b43b20f987a32bfabb1449860
alphmth <39932421+alphmth@users.noreply.github.com>
2022-01-10 20:36:45 +0500

    remove sqlite dependcies (#201)
    * remove sqlite dependcies
    
    * removed sql package for security reasons
    
    Co-authored-by: alphmth <>

--- packages/sqlite/src/index.js
+++ packages/sqlite/src/index.js
@@ -1,11 +1,13 @@
 'use strict';
 
-const KeyvSql = require('@keyv/sql');
+const EventEmitter = require('events');
 const sqlite3 = require('sqlite3');
 const pify = require('pify');
 
-class KeyvSqlite extends KeyvSql {
+class KeyvSqlite extends EventEmitter {
 	constructor(options) {
+		super();
+		this.ttlSupport = false;
 		options = Object.assign({
 			dialect: 'sqlite',
 			uri: 'sqlite://:memory:',
@@ -27,7 +29,61 @@ class KeyvSqlite extends KeyvSql {
 		})
 			.then(db => pify(db.all).bind(db));
 
-		super(options);
+		this.opts = Object.assign({
+			table: 'keyv',
+			keySize: 255,
+		}, options);
+
+		const createTable = `CREATE TABLE IF NOT EXISTS ${this.opts.table}(key VARCHAR(${Number(this.opts.keySize)}) PRIMARY KEY, value TEXT )`;
+
+		const connected = this.opts.connect()
+			.then(query => query(createTable).then(() => query))
+			.catch(error => this.emit('error', error));
+
+		this.query = sqlString => connected
+			.then(query => query(sqlString));
+	}
+
+	get(key) {
+		const select = `SELECT * FROM ${this.opts.table} WHERE key = '${key}'`;
+		return this.query(select)
+			.then(rows => {
+				const row = rows[0];
+				if (row === undefined) {
+					return undefined;
+				}
+
+				return row.value;
+			});
+	}
+
+	set(key, value) {
+		const upsert = `INSERT INTO ${this.opts.table} (key, value)
+			VALUES('${key}', '${value}') 
+			ON CONFLICT(key) 
+			DO UPDATE SET value=excluded.value;`;
+		return this.query(upsert);
+	}
+
+	delete(key) {
+		const select = `SELECT * FROM ${this.opts.table} WHERE key = '${key}'`;
+		const del = `DELETE FROM ${this.opts.table} WHERE key = '${key}'`;
+		return this.query(select)
+			.then(rows => {
+				const row = rows[0];
+				if (row === undefined) {
+					return false;
+				}
+
+				return this.query(del)
+					.then(() => true);
+			});
+	}
+
+	clear() {
+		const del = `DELETE FROM ${this.opts.table} WHERE key LIKE '${this.namespace}:%'`;
+		return this.query(del)
+			.then(() => undefined);
 	}
 }
 
