28705be98ce1bec02903c6b552a5f5d83b1cb827
alphmth <39932421+alphmth@users.noreply.github.com>
2022-01-14 20:43:19 +0500

    removed from mysql package (#203)
    * removed from mysql package
    
    * removed from postgres package
    
    * Revert "removed from postgres package"
    
    This reverts commit 1b3feb1d54a3d2d8f2fa0f183e2b1e24e3952a3d.
    
    Co-authored-by: alphmth <>

--- packages/mysql/src/index.js
+++ packages/mysql/src/index.js
@@ -1,10 +1,12 @@
 'use strict';
 
-const KeyvSql = require('@keyv/sql');
+const EventEmitter = require('events');
 const mysql = require('mysql2/promise');
 
-class KeyvMysql extends KeyvSql {
+class KeyvMysql extends EventEmitter {
 	constructor(options) {
+		super();
+		this.ttlSupport = false;
 		if (typeof options === 'string') {
 			options = { uri: options };
 		}
@@ -19,7 +21,62 @@ class KeyvMysql extends KeyvSql {
 			.then(connection => sql => connection.execute(sql)
 				.then(data => data[0]));
 
-		super(options);
+		this.opts = Object.assign({
+			table: 'keyv',
+			keySize: 255,
+		}, options);
+
+		const createTable = `CREATE TABLE IF NOT EXISTS ${this.opts.table}(id VARCHAR(${Number(this.opts.keySize)}) PRIMARY KEY, value TEXT )`;
+
+		const connected = this.opts.connect()
+			.then(query => query(createTable).then(() => query))
+			.catch(error => this.emit('error', error));
+
+		this.query = sqlString => connected
+			.then(query => query(sqlString));
+	}
+
+	get(key) {
+		const select = `SELECT * FROM ${this.opts.table} WHERE id = '${key}'`;
+		return this.query(select)
+			.then(rows => {
+				const row = rows[0];
+				if (row === undefined) {
+					return undefined;
+				}
+
+				return row.value;
+			});
+	}
+
+	set(key, value) {
+		const sql = `INSERT INTO ${this.opts.table} (id, value)
+			VALUES(?, ?) 
+			ON DUPLICATE KEY UPDATE value=?;`;
+		const insert = [key, value, value];
+		const upsert = mysql.format(sql, insert);
+		return this.query(upsert);
+	}
+
+	delete(key) {
+		const select = `SELECT * FROM ${this.opts.table} WHERE id = '${key}'`;
+		const del = `DELETE FROM ${this.opts.table} WHERE id = '${key}'`;
+		return this.query(select)
+			.then(rows => {
+				const row = rows[0];
+				if (row === undefined) {
+					return false;
+				}
+
+				return this.query(del)
+					.then(() => true);
+			});
+	}
+
+	clear() {
+		const del = `DELETE FROM ${this.opts.table} WHERE id LIKE '${this.namespace}:%'`;
+		return this.query(del)
+			.then(() => undefined);
 	}
 }
 
