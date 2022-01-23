45a807a6aaab884388b2884d60d5efab9b39b9da
Charlie Laabs <charlielaabs@gmail.com>
2022-01-23 12:58:58 -0600

    Convert sqlite3 to better-sqlite3 - Resolve single quote parsing issue in query

--- packages/keyv/test/test.js
+++ packages/keyv/test/test.js
@@ -1,11 +1,12 @@
-const EventEmitter = require('events');
 const test = require('ava');
-const keyvTestSuite = require('@keyv/test-suite').default;
-const { keyvOfficialTests } = require('@keyv/test-suite');
+const { keyvOfficialTests, default: keyvTestSuite } = require('@keyv/test-suite');
 const Keyv = require('this');
 const tk = require('timekeeper');
-const sqlite3 = require('sqlite3');
-const pify = require('pify');
+const KeyvSqlite = require('@keyv/sqlite');
+
+keyvOfficialTests(test, Keyv, 'sqlite://test/testdb.sqlite', 'sqlite://non/existent/database.sqlite');
+const store = () => new KeyvSqlite({ uri: 'sqlite://test/testdb.sqlite', busyTimeout: 3000 });
+keyvTestSuite(test, Keyv, store);
 
 test.serial('Keyv is a class', t => {
 	t.is(typeof Keyv, 'function');
@@ -127,91 +128,3 @@ test.serial('Keyv supports async serializer/deserializer', async t => {
 	await keyv.set('foo', 'bar');
 	t.is(await keyv.get('foo'), 'bar');
 });
-
-class TestAdapter extends EventEmitter {
-	constructor(options) {
-		super();
-		this.ttlSupport = false;
-		options = Object.assign({
-			dialect: 'sqlite',
-			uri: 'sqlite://:memory:',
-		}, options);
-		options.db = options.uri.replace(/^sqlite:\/\//, '');
-
-		options.connect = () => new Promise((resolve, reject) => {
-			const db = new sqlite3.Database(options.db, error => {
-				if (error) {
-					reject(error);
-				} else {
-					if (options.busyTimeout) {
-						db.configure('busyTimeout', options.busyTimeout);
-					}
-
-					resolve(db);
-				}
-			});
-		})
-			.then(db => pify(db.all).bind(db));
-
-		this.opts = Object.assign({
-			table: 'keyv',
-			keySize: 255,
-		}, options);
-
-		const createTable = `CREATE TABLE IF NOT EXISTS ${this.opts.table}(key VARCHAR(${Number(this.opts.keySize)}) PRIMARY KEY, value TEXT )`;
-
-		const connected = this.opts.connect()
-			.then(query => query(createTable).then(() => query))
-			.catch(error => this.emit('error', error));
-
-		this.query = sqlString => connected
-			.then(query => query(sqlString));
-	}
-
-	get(key) {
-		const select = `SELECT * FROM ${this.opts.table} WHERE key = '${key}'`;
-		return this.query(select)
-			.then(rows => {
-				const row = rows[0];
-				if (row === undefined) {
-					return undefined;
-				}
-
-				return row.value;
-			});
-	}
-
-	set(key, value) {
-		const upsert = `INSERT INTO ${this.opts.table} (key, value)
-			VALUES('${key}', '${value}') 
-			ON CONFLICT(key) 
-			DO UPDATE SET value=excluded.value;`;
-		return this.query(upsert);
-	}
-
-	delete(key) {
-		const select = `SELECT * FROM ${this.opts.table} WHERE key = '${key}'`;
-		const del = `DELETE FROM ${this.opts.table} WHERE key = '${key}'`;
-		return this.query(select)
-			.then(rows => {
-				const row = rows[0];
-				if (row === undefined) {
-					return false;
-				}
-
-				return this.query(del)
-					.then(() => true);
-			});
-	}
-
-	clear() {
-		const del = `DELETE FROM ${this.opts.table} WHERE key LIKE '${this.namespace}:%'`;
-		return this.query(del)
-			.then(() => undefined);
-	}
-}
-
-keyvOfficialTests(test, Keyv, 'sqlite://test/testdb.sqlite', 'sqlite://non/existent/database.sqlite');
-
-const store = () => new TestAdapter({ uri: 'sqlite://test/testdb.sqlite', busyTimeout: 3000 });
-keyvTestSuite(test, Keyv, store);
