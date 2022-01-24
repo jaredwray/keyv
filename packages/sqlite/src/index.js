33e1a60d0411971efefc1718e64df55cab88c4cc
Charlie Laabs <charlielaabs@gmail.com>
2022-01-24 14:09:04 -0600

    Convert sqlite3 to better-sqlite3 (#210)
    - Resolve single quote parsing issue in query

--- packages/sqlite/src/index.js
+++ packages/sqlite/src/index.js
@@ -1,8 +1,7 @@
 'use strict';
 
 const EventEmitter = require('events');
-const sqlite3 = require('sqlite3');
-const pify = require('pify');
+const Database = require('better-sqlite3');
 
 class KeyvSqlite extends EventEmitter {
 	constructor(options) {
@@ -14,21 +13,6 @@ class KeyvSqlite extends EventEmitter {
 		}, options);
 		options.db = options.uri.replace(/^sqlite:\/\//, '');
 
-		options.connect = () => new Promise((resolve, reject) => {
-			const db = new sqlite3.Database(options.db, error => {
-				if (error) {
-					reject(error);
-				} else {
-					if (options.busyTimeout) {
-						db.configure('busyTimeout', options.busyTimeout);
-					}
-
-					resolve(db);
-				}
-			});
-		})
-			.then(db => pify(db.all).bind(db));
-
 		this.opts = Object.assign({
 			table: 'keyv',
 			keySize: 255,
@@ -36,54 +20,54 @@ class KeyvSqlite extends EventEmitter {
 
 		const createTable = `CREATE TABLE IF NOT EXISTS ${this.opts.table}(key VARCHAR(${Number(this.opts.keySize)}) PRIMARY KEY, value TEXT )`;
 
-		const connected = this.opts.connect()
-			.then(query => query(createTable).then(() => query))
-			.catch(error => this.emit('error', error));
+		const dbOptions = {};
+		if (options.busyTimeout) {
+			dbOptions.timeout = options.busyTimeout;
+		}
 
-		this.query = sqlString => connected
-			.then(query => query(sqlString));
+		try {
+			this.db = new Database(options.db, dbOptions);
+			this.db.prepare(createTable).run();
+		} catch (error) {
+			setImmediate(() => this.emit('error', error));
+		}
 	}
 
 	get(key) {
-		const select = `SELECT * FROM ${this.opts.table} WHERE key = '${key}'`;
-		return this.query(select)
-			.then(rows => {
-				const row = rows[0];
-				if (row === undefined) {
-					return undefined;
-				}
+		const select = `SELECT * FROM ${this.opts.table} WHERE key = ?`;
+		const row = this.db.prepare(select).get(key);
+		if (row === undefined) {
+			return undefined;
+		}
 
-				return row.value;
-			});
+		return row.value;
 	}
 
 	set(key, value) {
 		const upsert = `INSERT INTO ${this.opts.table} (key, value)
-			VALUES('${key}', '${value}') 
+			VALUES(?, ?) 
 			ON CONFLICT(key) 
 			DO UPDATE SET value=excluded.value;`;
-		return this.query(upsert);
+		return this.db.prepare(upsert).run(key, value);
 	}
 
 	delete(key) {
-		const select = `SELECT * FROM ${this.opts.table} WHERE key = '${key}'`;
-		const del = `DELETE FROM ${this.opts.table} WHERE key = '${key}'`;
-		return this.query(select)
-			.then(rows => {
-				const row = rows[0];
-				if (row === undefined) {
-					return false;
-				}
+		const select = `SELECT * FROM ${this.opts.table} WHERE key = ?`;
+		const del = `DELETE FROM ${this.opts.table} WHERE key = ?`;
 
-				return this.query(del)
-					.then(() => true);
-			});
+		const row = this.db.prepare(select).get(key);
+		if (row === undefined) {
+			return false;
+		}
+
+		this.db.prepare(del).run(key);
+		return true;
 	}
 
 	clear() {
-		const del = `DELETE FROM ${this.opts.table} WHERE key LIKE '${this.namespace}:%'`;
-		return this.query(del)
-			.then(() => undefined);
+		const del = `DELETE FROM ${this.opts.table} WHERE key LIKE ?`;
+		this.db.prepare(del).run(`${this.namespace}:%`);
+		return undefined;
 	}
 }
 
