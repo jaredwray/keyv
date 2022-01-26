55e9816433629bc10131b79498a07cf41372e993
alphmth <>
2022-01-26 12:56:26 +0500

    remove unnecessary block

--- packages/mongo/src/index.js
+++ packages/mongo/src/index.js
@@ -258,7 +258,7 @@ class KeyvMongo extends EventEmitter {
 					$lte: new Date(Date.now() - (seconds * 1000)),
 				},
 			}).toArray()
-			.then(lastAccessedFiles => Promise.all(lastAccessedFiles.map(file => client.bucket.delete(file._id))).then(() => true);
+			.then(lastAccessedFiles => Promise.all(lastAccessedFiles.map(file => client.bucket.delete(file._id))).then(() => true));
 		});
 	}
 }
