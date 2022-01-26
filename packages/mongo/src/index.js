ce9caaab7767c169bccc7cb9c8a6e43c28ad34d0
alphmth <39932421+alphmth@users.noreply.github.com>
2022-01-26 23:16:09 +0500

    initial implementation (#215)
    * initial implementation
    
    * added gridfs support for set, get, clear methods
    
    * added unused and expires method with test
    
    * added unused and expires method with test
    
    * added all gridfs methods
    
    * remove unnecessary block
    
    * remove unnecessary block
    
    * remove fix argument from test command
    
    * linting fix
    
    Co-authored-by: alphmth <>

--- packages/mongo/src/index.js
+++ packages/mongo/src/index.js
@@ -1,10 +1,12 @@
 'use strict';
 
 const EventEmitter = require('events');
+const Buffer = require('buffer').Buffer;
 const mongoClient = require('mongodb').MongoClient;
+const GridFSBucket = require('mongodb').GridFSBucket;
 const pify = require('pify');
 
-const keyvMongoKeys = new Set(['url', 'collection', 'namespace', 'serialize', 'deserialize', 'uri']);
+const keyvMongoKeys = new Set(['url', 'collection', 'namespace', 'serialize', 'deserialize', 'uri', 'useGridFS']);
 class KeyvMongo extends EventEmitter {
 	constructor(url, options) {
 		super();
@@ -33,6 +35,12 @@ class KeyvMongo extends EventEmitter {
 			),
 		);
 
+		this.opts = Object.fromEntries(
+			Object.entries(this.opts).filter(
+				([k]) => keyvMongoKeys.has(k),
+			),
+		);
+
 		// Implementation from sql by lukechilds,
 		this.connect = new Promise(resolve => {
 			mongoClient.connect(this.opts.url, mongoOptions
@@ -42,37 +50,99 @@ class KeyvMongo extends EventEmitter {
 					}
 
 					this.db = client.db(this.opts.db);
-					this.store = this.db.collection(this.opts.collection);
-					this.store.createIndex(
-						{ key: 1 },
-						{
-							unique: true,
-							background: true,
-						},
-					);
-					this.store.createIndex(
-						{ expiresAt: 1 },
-						{
-							expireAfterSeconds: 0,
-							background: true,
-						},
-					);
+					if (this.opts.useGridFS) {
+						this.bucket = new GridFSBucket(this.db, {
+							readPreference: this.opts.readPreference || 'primary',
+							bucketName: this.opts.collection,
+						});
+						this.store = this.db.collection(this.opts.collection + '.files');
+						this.store.createIndex({
+							filename: 'hashed',
+						});
+						this.store.createIndex({
+							uploadDate: -1,
+						});
+						this.store.createIndex({
+							'metadata.expiresAt': 1,
+						});
+						this.store.createIndex({
+							'metadata.lastAccessed': 1,
+						});
 
-					for (const method of [
-						'updateOne',
-						'findOne',
-						'deleteOne',
-						'deleteMany',
-					]) {
-						this.store[method] = pify(this.store[method].bind(this.store));
+						for (const method of [
+							'updateOne',
+						]) {
+							this.store[method] = pify(this.store[method].bind(this.store));
+						}
+
+						for (const method of [
+							'find',
+							'drop',
+						]) {
+							this.bucket[method] = pify(this.bucket[method].bind(this.bucket));
+						}
+
+						resolve({ bucket: this.bucket, store: this.store, db: this.db });
+					} else {
+						this.store = this.db.collection(this.opts.collection);
+						this.store.createIndex(
+							{ key: 1 },
+							{
+								unique: true,
+								background: true,
+							},
+						);
+						this.store.createIndex(
+							{ expiresAt: 1 },
+							{
+								expireAfterSeconds: 0,
+								background: true,
+							},
+						);
+
+						for (const method of [
+							'updateOne',
+							'findOne',
+							'deleteOne',
+							'deleteMany',
+						]) {
+							this.store[method] = pify(this.store[method].bind(this.store));
+						}
+
+						resolve(this.store);
 					}
-
-					resolve(this.store);
 				});
 		});
 	}
 
 	get(key) {
+		if (this.opts.useGridFS) {
+			return this.connect.then(client => {
+				client.store.updateOne({
+					filename: key,
+				}, {
+					$set: {
+						'metadata.lastAccessed': new Date(),
+					},
+				});
+
+				const stream = client.bucket.openDownloadStreamByName(key);
+				return new Promise(resolve => {
+					let resp = [];
+					stream.on('error', () => resolve());
+
+					stream.on('end', () => {
+						resp = Buffer.concat(resp).toString('utf-8');
+						resolve(resp);
+					});
+
+					stream.on('data', chunk => {
+						resp.push(chunk);
+					});
+				});
+			});
+		}
+
 		return this.connect.then(store =>
 			store.findOne({ key: { $eq: key } }).then(doc => {
 				if (!doc) {
@@ -86,6 +156,25 @@ class KeyvMongo extends EventEmitter {
 
 	set(key, value, ttl) {
 		const expiresAt = typeof ttl === 'number' ? new Date(Date.now() + ttl) : null;
+
+		if (this.opts.useGridFS) {
+			return this.connect.then(client => {
+				const stream = client.bucket.openUploadStream(key, {
+					metadata: {
+						expiresAt,
+						lastAccessed: new Date(),
+					},
+				});
+
+				return new Promise(resolve => {
+					stream.on('finish', () => {
+						resolve(stream);
+					});
+					stream.end(value);
+				});
+			});
+		}
+
 		return this.connect.then(store =>
 			store.updateOne(
 				{ key: { $eq: key } },
@@ -100,6 +189,18 @@ class KeyvMongo extends EventEmitter {
 			return Promise.resolve(false);
 		}
 
+		if (this.opts.useGridFS) {
+			return this.connect.then(client => {
+				const connection = client.db;
+				const bucket = new GridFSBucket(connection, {
+					bucketName: this.opts.collection,
+				});
+				return bucket.find({ filename: key }).toArray()
+					.then(files => client.bucket.delete(files[0]._id).then(() => true))
+					.catch(() => false);
+			});
+		}
+
 		return this.connect.then(store =>
 			store
 				.deleteOne({ key: { $eq: key } })
@@ -108,6 +209,10 @@ class KeyvMongo extends EventEmitter {
 	}
 
 	clear() {
+		if (this.opts.useGridFS) {
+			return this.connect.then(client => client.bucket.drop().then(() => undefined));
+		}
+
 		return this.connect.then(store =>
 			store
 				.deleteMany({
@@ -115,6 +220,46 @@ class KeyvMongo extends EventEmitter {
 				})
 				.then(() => undefined),
 		);
+	}
+
+	clearExpired() {
+		if (!this.opts.useGridFS) {
+			return false;
+		}
+
+		return this.connect.then(client => {
+			const connection = client.db;
+			const bucket = new GridFSBucket(connection, {
+				bucketName: this.opts.collection,
+			});
+
+			return bucket.find({
+				'metadata.expiresAt': {
+					$lte: new Date(Date.now()),
+				},
+			}).toArray()
+				.then(expiredFiles => Promise.all(expiredFiles.map(file => client.bucket.delete(file._id))).then(() => true));
+		});
+	}
+
+	clearUnusedFor(seconds) {
+		if (!this.opts.useGridFS) {
+			return false;
+		}
+
+		return this.connect.then(client => {
+			const connection = client.db;
+			const bucket = new GridFSBucket(connection, {
+				bucketName: this.opts.collection,
+			});
+
+			return bucket.find({
+				'metadata.lastAccessed': {
+					$lte: new Date(Date.now() - (seconds * 1000)),
+				},
+			}).toArray()
+				.then(lastAccessedFiles => Promise.all(lastAccessedFiles.map(file => client.bucket.delete(file._id))).then(() => true));
+		});
 	}
 }
 
--- packages/mongo/test/test.js
+++ packages/mongo/test/test.js
@@ -35,6 +35,15 @@ test('Collection option merges into default options', t => {
 	});
 });
 
+test('useGridFS option merges into default options', t => {
+	const store = new KeyvMongo({ useGridFS: true, collection: 'foo' });
+	t.deepEqual(store.opts, {
+		url: mongoURL,
+		useGridFS: true,
+		collection: 'foo',
+	});
+});
+
 test('Collection option merges into default options if URL is passed', t => {
 	const store = new KeyvMongo(mongoURL, { collection: 'foo' });
 	t.deepEqual(store.opts, {
@@ -49,6 +58,102 @@ test('.delete() with no args doesn\'t empty the collection', async t => {
 });
 
 test('.delete() with key as number', async t => {
-	const store = new KeyvMongo(mongoURL, { collection: 'foo' }); // Make sure we don't actually connect
+	const store = new KeyvMongo(mongoURL, { collection: 'foo' });
 	t.false(await store.delete(123));
+});
+
+test.serial('Stores value in GridFS', async t => {
+	const store = new KeyvMongo(Object.assign({
+		useGridFS: true,
+	}, options));
+	const result = await store.set('key1', 'keyv1', 0);
+	const get = await store.get('key1');
+	t.is(result.filename, 'key1');
+	t.is(get, 'keyv1');
+});
+
+test.serial('Gets value from GridFS', async t => {
+	const store = new KeyvMongo(Object.assign({
+		useGridFS: true,
+	}, options));
+	const result = await store.get('key1');
+	t.is(result, 'keyv1');
+});
+
+test.serial('Deletes value from GridFS', async t => {
+	const store = new KeyvMongo(Object.assign({
+		useGridFS: true,
+	}, options));
+	const result = await store.delete('key1');
+	t.is(result, true);
+});
+
+test.serial('Deletes non existent value from GridFS', async t => {
+	const store = new KeyvMongo(Object.assign({
+		useGridFS: true,
+	}, options));
+	const result = await store.delete('no-existent-value');
+	t.is(result, false);
+});
+
+test.serial('Stores value with TTL in GridFS', async t => {
+	const store = new KeyvMongo(Object.assign({
+		useGridFS: true,
+	}, options));
+	const result = await store.set('key1', 'keyv1', 0);
+	t.is(result.filename, 'key1');
+});
+
+test.serial('Clears expired value from GridFS', async t => {
+	const store = new KeyvMongo(Object.assign({
+		useGridFS: true,
+	}, options));
+	const cleared = await store.clearExpired();
+	t.is(cleared, true);
+});
+
+test.serial('Clears unused files from GridFS', async t => {
+	const store = new KeyvMongo(Object.assign({
+		useGridFS: true,
+	}, options));
+	const cleared = await store.clearUnusedFor(5);
+	t.is(cleared, true);
+});
+
+test.serial('Clears expired value only when GridFS options is true', async t => {
+	const store = new KeyvMongo(Object.assign(options));
+	const cleared = await store.clearExpired();
+	t.is(cleared, false);
+});
+
+test.serial('Clears unused files only when GridFS options is true', async t => {
+	const store = new KeyvMongo(Object.assign(options));
+	const cleared = await store.clearUnusedFor(5);
+	t.is(cleared, false);
+});
+
+test.serial('Gets non-existent file and return should be undefined', async t => {
+	const store = new KeyvMongo(Object.assign({
+		useGridFS: true,
+	}, options));
+	const result = await store.get('non-existent-file');
+	t.is(typeof result, 'undefined');
+});
+
+test.serial('Non-string keys are not permitted in delete', async t => {
+	const store = new KeyvMongo(Object.assign({
+		useGridFS: true,
+	}, options));
+	const result = await store.delete({
+		ok: true,
+	});
+	t.is(result, false);
+});
+
+test.serial('Clears entire cache store', async t => {
+	const store = new KeyvMongo(Object.assign({
+		useGridFS: true,
+	}, options));
+	const result = await store.clear();
+	t.is(typeof result, 'undefined');
 });
