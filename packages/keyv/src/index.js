81e924a075b33102e20c5e1902a48b8ad66411d3
alphmth <>
2022-02-09 14:27:47 +0500

    lint

--- packages/keyv/src/index.js
+++ packages/keyv/src/index.js
@@ -73,8 +73,8 @@ class Keyv extends EventEmitter {
 		// Attach iterators
 		if (typeof this.opts.store[Symbol.iterator] === 'function' && this.opts.store instanceof Map) {
 			this.iterator = generateIterator(this.opts.store);
-		} else if (typeof this.opts.store.iterator === 'function' && this.opts.store.opts && 
-					(this.opts.store.opts.dialect === 'sqlite' || this.opts.store.opts.dialect === 'postgres')) { // Iterator only supported for sqlite for now
+		} else if (typeof this.opts.store.iterator === 'function' && this.opts.store.opts
+					&& (this.opts.store.opts.dialect === 'sqlite' || this.opts.store.opts.dialect === 'postgres')) { // Iterator only supported for sqlite for now
 			this.iterator = generateIterator(this.opts.store.iterator.bind(this.opts.store));
 		}
 	}
