ca13cb71833e8f8d3f4ea1038f36db786433fe6d
alphmth <>
2022-02-25 22:01:19 +0500

    delete all for postgres

--- packages/postgres/test/test.js
+++ packages/postgres/test/test.js
@@ -10,3 +10,16 @@ const store = () => new KeyvPostgres({ uri: 'postgresql://postgres:postgres@loca
 keyvTestSuite(test, Keyv, store);
 keyvIteratorTests(test, Keyv, store);
 
+test.serial('.deleteMany([keys]) should delete multiple key', async t => {
+	const keyv = new KeyvPostgres({ uri: 'postgresql://postgres:postgres@localhost:5432/keyv_test' });
+	await keyv.set('foo', 'bar');
+	await keyv.set('foo1', 'bar1');
+	await keyv.set('foo2', 'bar2');
+	t.is(await keyv.deleteMany(['foo', 'foo1', 'foo2']), true);
+});
+
+test.serial('.deleteMany([keys]) with nonexistent keys resolves to false', async t => {
+	const keyv = new KeyvPostgres({ uri: 'postgresql://postgres:postgres@localhost:5432/keyv_test' });
+	t.is(await keyv.deleteMany(['foo', 'foo1', 'foo2']), false);
+});
+
