7746afab031b8f6d460bd78b4d36dc4787bc9e9e
alphmth <>
2022-03-18 14:36:08 +0500

    refactor some types declaration for sqlite

--- packages/sqlite/src/index.d.ts
+++ packages/sqlite/src/index.d.ts
@@ -8,14 +8,14 @@ declare class KeyvSqlite extends EventEmitter {
 	opts: any;
 	db: Database;
 	constructor(options?: string | KeyvSqlite.Options);
-	get(key: any): any;
-	getMany(keys: any): any[];
-	set(key: string, value: any): Promise<any>;
+	get(key: string): Promise<string | undefined>;
+	getMany(keys: string[]): Promise<string[] | undefined>;
+	set(key: string, value: string | undefined): Promise<any>;
 	delete(key: string): boolean;
 	deleteMany(keys: string[]): boolean;
 	clear(): Promise<void>;
-	iterator(namespace: any): AsyncGenerator<any, void, any>;
-	has(key: any): boolean;
+	iterator(namespace: string | undefined): AsyncGenerator<any, void, any>;
+	has(key: string): boolean;
 }
 
 declare namespace KeyvSqlite {
