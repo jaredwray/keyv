a2341d2e5187ce6769ba0e39289b02194f7df203
alphmth <>
2022-04-12 20:59:50 +0500

    fixed memchache constructor issue

--- packages/memcache/src/index.d.ts
+++ packages/memcache/src/index.d.ts
@@ -1,15 +1,15 @@
 import {EventEmitter} from 'events';
 import { Store } from 'keyv';
 
-declare class KeyvMemcache extends EventEmitter implements Store<TValue> {
+declare class KeyvMemcache extends EventEmitter implements Store<string | undefined> {
     ttlSupport: boolean;
     namespace?: string | undefined;
 
     constructor(uri?: string)
     constructor(opts?: KeyvMemcache.Options);
 
-    get(key: string): Promise<TValue | undefined>;
-    getMany(keys: string[]): Promise<TValue[] | undefined>;
+    get(key: string): Promise<string | undefined>;
+    getMany(keys: string[]): Promise<string[] | undefined>;
     set(key: string, value: TValue | undefined): Promise<any>;
     delete(key: string): boolean;
     deleteMany(keys: string[]): boolean;
