738a21c270ae309885d603a5857f36310bce38e7
alphmth <39932421+alphmth@users.noreply.github.com>
2022-04-12 23:25:59 +0500

    Bug 278 (#283)
    * fixed type issue in redis
    
    * lint
    
    * fixed redis type issue
    
    Co-authored-by: alphmth <>

--- packages/redis/src/index.d.ts
+++ packages/redis/src/index.d.ts
@@ -1,14 +1,14 @@
 import {EventEmitter} from 'events';
 import {Store} from 'keyv';
-import {ClientOpts} from 'redis';
+import {Redis, Cluster} from 'ioredis';
 
 declare class KeyvRedis extends EventEmitter implements Store<string | undefined> {
 	readonly ttlSupport: false;
 	namespace?: string | undefined;
 	opts: Record<string, unknown>;
 	redis: any;
-	constructor(options?: KeyvRedis.Options);
-	constructor(uri: string, options?: KeyvRedis.Options);
+	constructor(options?: KeyvRedis.Options | Redis | Cluster);
+	constructor(uri: string | Redis | Cluster, options?: KeyvRedis.Options);
 	get(key: string): Promise<string | undefined>;
 	getMany(keys: string[]): Promise<string[] | undefined>;
 	set(key: string, value: string | undefined, ttl?: number): Promise<any>;
