8898ca030799f9ba1270bc1e605ed25779a95090
Jared Wray <me@jaredwray.com>
2022-06-17 15:55:50 -0700

    adding in xo support for memcache (#364)

--- packages/memcache/src/index.d.ts
+++ packages/memcache/src/index.d.ts
@@ -1,28 +1,29 @@
-import {EventEmitter} from 'events';
-import { Store } from 'keyv';
+/* eslint-disable @typescript-eslint/no-redundant-type-constituents */
+import {EventEmitter} from 'node:events';
+import {Store} from 'keyv';
 
 declare class KeyvMemcache extends EventEmitter implements Store<string | undefined> {
-    ttlSupport: boolean;
-    namespace?: string | undefined;
+	ttlSupport: boolean;
+	namespace?: string | undefined;
 
-    constructor(uri?: string)
-    constructor(opts?: KeyvMemcache.Options);
+	constructor(uri?: string);
+	constructor(options?: KeyvMemcache.Options);
 
-    get(key: string): Promise<string | undefined>;
-    getMany(keys: string[]): Promise<string[] | undefined>;
-    set(key: string, value: TValue | undefined): Promise<any>;
-    delete(key: string): boolean;
-    deleteMany(keys: string[]): boolean;
-    clear(): Promise<void>;
-    iterator(namespace: string | undefined): AsyncGenerator<any, void, any>;
-    has(key: string): boolean;
+	get(key: string): Promise<string | undefined>;
+	getMany(keys: string[]): Promise<string[] | undefined>;
+	set(key: string, value: TValue | undefined): Promise<any>;
+	delete(key: string): boolean;
+	deleteMany(keys: string[]): boolean;
+	clear(): Promise<void>;
+	iterator(namespace: string | undefined): AsyncGenerator<any, void, any>;
+	has(key: string): boolean;
 }
 
 declare namespace KeyvMemcache {
-    interface Options {
-        url?: string | undefined;
-        uri?: string | undefined;
-    }
+	interface Options {
+		url?: string | undefined;
+		uri?: string | undefined;
+	}
 }
 
 export = KeyvMemcache;
