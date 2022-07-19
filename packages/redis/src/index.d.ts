3bc146ce4286fe963422994af2a738e14bcb4b1f
alphmth <>
2022-07-19 21:10:06 +0500

    fixed type issues redis

--- packages/redis/src/index.d.ts
+++ packages/redis/src/index.d.ts
@@ -2,21 +2,23 @@ import {EventEmitter} from 'events';
 import {Store} from 'keyv';
 import {Redis, Cluster} from 'ioredis';
 
-declare class KeyvRedis extends EventEmitter implements Store<string | undefined> {
+declare class KeyvRedis extends EventEmitter implements Store<Value> {
 	readonly ttlSupport: false;
 	namespace?: string | undefined;
 	opts: Record<string, unknown>;
 	redis: any;
 	constructor(options?: KeyvRedis.Options | Redis | Cluster);
 	constructor(uri: string | Redis | Cluster, options?: KeyvRedis.Options);
-	get(key: string): Promise<string | undefined>;
-	getMany(keys: string[]): Promise<string[]>;
-	set(key: string, value: string | undefined, ttl?: number): Promise<any>;
-	delete(key: string): boolean;
+	get(key: string): Promise<Value>;
+	getMany?(
+		keys: string[]
+	): Array<StoredData<Value>> | Promise<Array<StoredData<Value>>> | undefined;
+	set(key: string, value: Value, ttl?: number): any;
+	delete(key: string): boolean | Promise<boolean>;
 	deleteMany(keys: string[]): boolean;
-	clear(): Promise<void>;
+	clear(): void | Promise<void>;
 	iterator(namespace: string | undefined): AsyncGenerator<any, void, any>;
-	has(key: string): boolean;
+	has?(key: string): boolean | Promise<boolean>;
 }
 declare namespace KeyvRedis {
 	interface Options {
