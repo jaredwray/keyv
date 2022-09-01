70f8f4407b15c360250a3d452bb753a86a68e2f1
alphmth <>
2022-09-01 22:19:46 +0500

    support pako

--- packages/compress-gzip/src/index.js
+++ packages/compress-gzip/src/index.js
@@ -1,28 +1,29 @@
-const {gzip, ungzip} = require('node-gzip');
+const pako = require('pako');
 const JSONB = require('json-buffer');
 
 class KeyvGzip {
 	constructor(options) {
 		this.opts = {
+			to: 'string',
 			...options,
 		};
 
-		this.opts.compress = gzip;
-		this.opts.decompress = ungzip;
-		this.opts.serialize = async ({value, expires}) => JSONB.stringify({value: await gzip(value, this.opts), expires});
+		this.opts.compress = pako.deflate;
+		this.opts.decompress = pako.inflate;
+		this.opts.serialize = async ({value, expires}) => JSONB.stringify({value: await this.opts.compress(value, this.opts), expires});
 		this.opts.deserialize = async data => {
 			const {value, expires} = JSONB.parse(data);
-			const value_ = await ungzip(value, this.opts);
-			return {value: value_.toString(), expires};
+			const value_ = await this.opts.decompress(value, this.opts);
+			return {value: value_, expires};
 		};
 	}
 
 	compress(value, options) {
-		return this.opts.compress(value, options);
+		return this.opts.compress(value, {...this.opts, ...options});
 	}
 
 	decompress(value, options) {
-		return this.opts.decompress(value, options);
+		return this.opts.decompress(value, {...this.opts, ...options});
 	}
 }
 
