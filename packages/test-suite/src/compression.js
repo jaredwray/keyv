eaaf21e218c4458f3fc0d4032db16f52b2767f28
alphmth <>
2022-10-12 21:48:42 +0500

    added generic compression test file

---
+++ packages/test-suite/src/compression.js
@@ -0,0 +1,40 @@
+const Keyv = require('keyv');
+
+const keyvCompressionTests = (test, compression) => {
+	let keyv;
+	test.beforeEach(async () => {
+		keyv = new Keyv({
+			store: new Map(),
+			compression,
+		});
+		await keyv.clear();
+	});
+
+	test.serial('compression/decompression using default options', async t => {
+		const compressed = await compression.compress('whatever');
+		t.not(compressed, 'whatever');
+		const decompressed = await compression.decompress(compressed);
+		t.is(decompressed, 'whatever');
+	});
+
+	// Test serialize compression
+	test('serialize compression', async t => {
+		const json = await compression.serialize({value: 'whatever'});
+		t.not(JSON.parse(json).value, 'whatever');
+	});
+
+	// Test deserialize compression
+	test('deserialize compression', async t => {
+		const json = await compression.serialize({value: 'whatever'});
+		const djson = await compression.deserialize(json);
+		t.deepEqual(djson, {expires: undefined, value: 'whatever'});
+	});
+
+	test('compress/decompress with main keyv', async t => {
+		const keyv = new Keyv({store: new Map(), compression: compression});
+		await keyv.set('foo', 'bar');
+		t.is(await keyv.get('foo'), 'bar');
+	});
+};
+
+module.exports = keyvCompressionTests;
