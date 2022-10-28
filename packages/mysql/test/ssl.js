a027949ca5373bf7a0091846c82b880b40c45eca
alphmth <>
2022-10-28 19:37:39 +0500

    updated cert file

--- packages/mysql/test/ssl.js
+++ packages/mysql/test/ssl.js
@@ -7,7 +7,7 @@ const {endPool} = require('../src/pool.js');
 const options = {
 	ssl: {
 		rejectUnauthorized: true,
-		ca: fs.readFileSync(path.join(__dirname, '/certs/ca-cert.pem')),
+		ca: fs.readFileSync(path.join(__dirname, '/certs/ca.pem')),
 		key: fs.readFileSync(path.join(__dirname, '/certs/client-key.pem')),
 		cert: fs.readFileSync(path.join(__dirname, '/certs/client-cert.pem')),
 	},
