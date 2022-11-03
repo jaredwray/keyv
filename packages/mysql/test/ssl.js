5c6ca18bcf71695669b114f5b2e046663bd7f441
alphmth <39932421+alphmth@users.noreply.github.com>
2022-11-04 04:50:21 +0500

    ssl configs for mysql (#532)
    * ssl configs for mysql
    
    * docker file revert
    
    * updated cert file
    
    * updated cert file
    
    * updated mysql version
    
    * updated mysql version
    
    * update docker file
    
    * fix ssl certificate issue
    
    * update docker file
    
    * update docker file
    
    * update docker file
    
    * update docker file
    
    * update docker file
    
    * update docker file
    
    * lint
    
    * sh256 certficates
    
    * sh256 certficates
    
    * updated readme
    
    Co-authored-by: alphmth <>

---
+++ packages/mysql/test/ssl.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+const test = require('ava');
+const KeyvMysql = require('this');
+const {endPool} = require('../src/pool.js');
+
+const options = {
+	ssl: {
+		rejectUnauthorized: false,
+		ca: fs.readFileSync(path.join(__dirname, '/certs/ca.pem')).toString(),
+		key: fs.readFileSync(path.join(__dirname, '/certs/client-key.pem')).toString(),
+		cert: fs.readFileSync(path.join(__dirname, '/certs/client-cert.pem')).toString(),
+	},
+};
+
+test.serial('throws if ssl is not used', async t => {
+	try {
+		const keyv = new KeyvMysql({uri: 'mysql://root@localhost:3307/keyv_test'});
+		await keyv.get('foo');
+		t.fail();
+	} catch {
+		t.pass();
+	} finally {
+		endPool();
+	}
+});
+
+test.serial('set with ssl ', async t => {
+	const keyv = new KeyvMysql({uri: 'mysql://root@localhost:3307/keyv_test', ...options});
+	await keyv.set('key', 'value');
+	t.is(await keyv.get('key'), 'value');
+});
