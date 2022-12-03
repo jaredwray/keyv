35d1dcec82dc8e7e4ea2a0caf734bef2ee937ec4
Jared Wray <me@jaredwray.com>
2022-12-03 14:52:35 -0800

    redis - upgrading typescript to 4.9.3 (#598)

--- packages/redis/src/index.d.ts
+++ packages/redis/src/index.d.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/consistent-type-definitions */
 import {EventEmitter} from 'events';
-import type {Store, StoredData} from 'keyv';
-import type {Redis, Cluster} from 'ioredis';
+import type{Store, StoredData} from 'keyv';
+import type{Redis, Cluster} from 'ioredis';
 
 declare class KeyvRedis<Value=any> extends EventEmitter implements Store<Value> {
 	readonly ttlSupport: false;
