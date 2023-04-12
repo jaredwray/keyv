a719eaa960e08946468672b4d3bc9471c8336cc8
Christian Llontop <christian@rhinolabs.co>
2023-04-12 00:17:05 -0500

    refactor typescript class

--- packages/etcd/src/index.ts
+++ packages/etcd/src/index.ts
@@ -1,7 +1,7 @@
 import {EventEmitter} from 'events';
+import type {Lease} from 'etcd3';
 import {Etcd3} from 'etcd3';
-import {handleAll, retry, ExponentialBackoff} from 'cockatiel';
-import type {Lease, PutBuilder} from 'etcd3';
+import {ExponentialBackoff, handleAll, retry} from 'cockatiel';
 import type {Store, StoredData} from 'keyv';
 
 type KeyvEtcdOptions = {
@@ -9,32 +9,43 @@ type KeyvEtcdOptions = {
 	uri?: string;
 	ttl?: number;
 	busyTimeout?: number;
-} | string;
+};
 
-class KeyvEtcd<Value=any> extends EventEmitter implements Store<Value> {
+type GetOutput<Value> = Value | Promise<Value | undefined> | undefined;
+
+class KeyvEtcd<Value = any> extends EventEmitter implements Store<Value> {
 	public ttlSupport: boolean;
 	public opts: KeyvEtcdOptions;
 	public client: Etcd3;
 	public lease?: Lease;
 	public namespace?: string;
 
-	constructor(url?: KeyvEtcdOptions, options?: KeyvEtcdOptions) {
+	constructor(url?: KeyvEtcdOptions | string, options?: KeyvEtcdOptions) {
 		super();
-		this.ttlSupport = false;
+
+		this.ttlSupport = typeof options?.ttl === 'number';
+
+		url = url ?? {};
+
+		if (typeof url === 'string') {
+			url = {url};
+		}
+
+		if (url.uri) {
+			url = {url: url.uri, ...url};
+		}
+
+		if (url.ttl) {
+			this.ttlSupport = typeof url.ttl === 'number';
+		}
+
 		this.opts = {
 			url: '127.0.0.1:2379',
-			...(typeof options === 'string' ? {url: options} : options),
+			...url,
+			...options,
 		};
 
-		if (this.opts.uri) {
-			this.opts.url = this.opts.uri;
-		}
-
-		if (this.opts.ttl) {
-			this.ttlSupport = typeof this.opts.ttl === 'number';
-		}
-
-		this.opts.url = this.opts.url?.replace(/^etcd:\/\//, '');
+		this.opts.url = this.opts.url!.replace(/^etcd:\/\//, '');
 
 		const policy = retry(handleAll, {backoff: new ExponentialBackoff()});
 		policy.onFailure(error => {
@@ -42,85 +53,94 @@ class KeyvEtcd<Value=any> extends EventEmitter implements Store<Value> {
 		});
 
 		this.client = new Etcd3({
-			hosts: this.opts.url!,
+			hosts: this.opts.url,
 			faultHandling: {
 				// @ts-expect-error - iPolicy
 				host: () => policy,
-				// @ts-expect-error  - iPolicy
+				// @ts-expect-error - iPolicy
 				global: policy,
 			},
 		});
 
+		// Https://github.com/microsoft/etcd3/issues/105
 		this.client.getRoles().catch(error => this.emit('error', error));
 
 		if (this.ttlSupport) {
-			this.lease = this.client.lease((this.opts.ttl!) / 1000, {
+			this.lease = this.client.lease(this.opts.ttl! / 1000, {
 				autoKeepAlive: false,
 			});
 		}
 	}
 
-	async get(key: string): Promise<Value> {
-		return this.client.get(key) as unknown as Promise<Value>;
+	get(key: string): GetOutput<Value> {
+		return this.client.get(key) as unknown as GetOutput<Value>;
 	}
 
-	async getMany(keys: string[]): Promise<Array<StoredData<Value>>> {
+	getMany(keys: string[]): Promise<Array<StoredData<Value>>> {
 		const promises = [];
 		for (const key of keys) {
 			promises.push(this.get(key));
 		}
 
-		const values = await Promise.allSettled(promises);
-		const data: Array<StoredData<Value>> = [];
-		for (const value of values) {
-			// @ts-expect-error - value is an object
-			if (value.value === null) {
-				data.push(undefined);
-			} else {
-				// @ts-expect-error - value is an object
-				data.push(value.value);
-			}
+		return Promise.allSettled(promises)
+			.then(values => {
+				const data: Array<StoredData<Value>> = [];
+				for (const value of values) {
+					// @ts-expect-error - value is an object
+					if (value.value === null) {
+						data.push(undefined);
+					} else {
+						// @ts-expect-error - value is an object
+						data.push(value.value);
+					}
+				}
+
+				return data;
+			});
+	}
+
+	set(key: string, value: Value) {
+		let client: 'lease' | 'client' = 'client';
+
+		if (this.opts.ttl) {
+			client = 'lease';
 		}
 
-		return data;
+		// @ts-expect-error - Value needs to be number, string or buffer
+		return this[client]!.put(key).value(value);
 	}
 
-	set(key: string, value: Value): PutBuilder {
-		// @ts-expect-error - value needs extends Buffer
-		return this.opts.ttl ? this.lease!.put(key).value(value) : this.client.put(key).value(value);
-	}
-
-	async delete(key: string): Promise<boolean> {
+	delete(key: string): Promise<boolean> {
 		if (typeof key !== 'string') {
-			return false;
+			return Promise.resolve(false);
 		}
 
 		return this.client.delete().key(key).then(key => key.deleted !== '0');
 	}
 
-	async deleteMany(keys: string[]): Promise<boolean> {
+	deleteMany(keys: string[]): Promise<boolean> {
 		const promises = [];
 		for (const key of keys) {
 			promises.push(this.delete(key));
 		}
 
-		return Promise.allSettled(promises)
-			.then(values => values.every(Boolean));
+		// @ts-expect-error - x is an object
+		return Promise.allSettled(promises).then(values => values.every(x => x.value === true));
 	}
 
-	async clear(): Promise<void> {
+	clear(): Promise<void> {
 		const promise = this.namespace
 			? this.client.delete().prefix(this.namespace)
 			: this.client.delete().all();
 		return promise.then(() => undefined);
 	}
 
-	async has(key: string): Promise<boolean> {
+	has(key: string): Promise<boolean> {
 		return this.client.get(key).exists();
 	}
 
-	disconnect(): void {
-		this.client.close();
+	disconnect() {
+		return this.client.close();
 	}
 }
 
