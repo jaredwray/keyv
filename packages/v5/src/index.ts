500634734697d42df7a3a6147918eccba80d1f14
Jared Wray <me@jaredwray.com>
2023-06-09 17:12:13 -0700

    adding in interfaces

--- packages/v5/src/index.ts
+++ packages/v5/src/index.ts
@@ -1,4 +1,28 @@
 /* eslint-disable @typescript-eslint/no-extraneous-class */
+
+interface IStorageAdapter {
+    get(key: string): Promise<string | undefined>;
+    getMany(keys: string[]): Promise<(string | undefined)[]>;
+    set(key: string, value: string): Promise<void>;
+    delete(key: string): Promise<boolean>;
+    clear(): Promise<void>;
+}
+
+interface ICompressionAdapter {
+    compress(data: string): Promise<string>;
+    decompress(data: string): Promise<string>;
+}
+
+interface IKeyvOptions {
+    namespace?:  Function | string;
+    ttl?: number;
+    adapter?: IStorageAdapter;
+    uri?: string;
+    compression?: ICompressionAdapter;
+    secondaryAdapter?: IStorageAdapter;
+    offlineMode?: boolean;
+}
+
 class Keyv {}
 
 export = Keyv;
