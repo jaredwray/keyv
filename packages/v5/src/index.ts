f55f366e7094d327e8c0ab9556d2d685f1102130
Jared Wray <me@jaredwray.com>
2023-06-10 13:49:19 -0700

    Update index.ts

--- packages/v5/src/index.ts
+++ packages/v5/src/index.ts
@@ -1,27 +1,29 @@
 /* eslint-disable @typescript-eslint/no-extraneous-class */
 
-interface IStorageAdapter {
-    get(key: string): Promise<string | undefined>;
-    getMany(keys: string[]): Promise<(string | undefined)[]>;
-    set(key: string, value: string): Promise<void>;
-    delete(key: string): Promise<boolean>;
-    clear(): Promise<void>;
-}
+type StorageAdapterType = {
+	get<T>(key: string): Promise<T | undefined>;
+	getMany(keys: string[]): Promise<Array<string | undefined>>;
+	set(key: string, value: string): Promise<void>;
+	delete(key: string): Promise<boolean>;
+	deleteMany(keys: string[]): Promise<boolean[]>;
+	clear(): Promise<void>;
+	disconnect?(): Promise<void>;
+};
 
-interface ICompressionAdapter {
-    compress(data: string): Promise<string>;
-    decompress(data: string): Promise<string>;
-}
+type CompressionAdapterType = {
+	compress(data: string): Promise<string>;
+	decompress(data: string): Promise<string>;
+};
 
-interface IKeyvOptions {
-    namespace?:  Function | string;
-    ttl?: number;
-    adapter?: IStorageAdapter;
-    uri?: string;
-    compression?: ICompressionAdapter;
-    secondaryAdapter?: IStorageAdapter;
-    offlineMode?: boolean;
-}
+type KeyvOptionsType = {
+	namespace?: string;
+	ttl?: number;
+	adapter?: StorageAdapterType;
+	uri?: string;
+	compression?: CompressionAdapterType;
+	secondaryAdapter?: StorageAdapterType;
+	offlineMode?: boolean;
+};
 
 class Keyv {}
 
