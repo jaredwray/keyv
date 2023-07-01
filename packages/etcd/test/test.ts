8bd91925d217def254ee8500ae2b898c1b6facb5
Christian Llontop <christian@rhinolabs.co>
2023-06-30 21:30:18 -0500

    add iterator on etcd adapter

--- packages/etcd/test/test.ts
+++ packages/etcd/test/test.ts
@@ -11,14 +11,14 @@ const store = () => new KeyvEtcd({uri: etcdUrl, busyTimeout: 3000});
 
 keyvTestSuite(test, Keyv, store);
 
-test('default options', t => {
+test.serial('default options', t => {
 	const store = new KeyvEtcd();
 	t.deepEqual(store.opts, {
 		url: '127.0.0.1:2379',
 	});
 });
 
-test('enable ttl using default url', t => {
+test.serial('enable ttl using default url', t => {
 	const store = new KeyvEtcd({ttl: 1000});
 	t.deepEqual(store.opts, {
 		url: '127.0.0.1:2379',
@@ -27,7 +27,7 @@ test('enable ttl using default url', t => {
 	t.is(store.ttlSupport, true);
 });
 
-test('disable ttl using default url', t => {
+test.serial('disable ttl using default url', t => {
 	// @ts-expect-error - ttl is not a number, just for test
 	const store = new KeyvEtcd({ttl: true});
 	t.deepEqual(store.opts, {
@@ -37,7 +37,7 @@ test('disable ttl using default url', t => {
 	t.is(store.ttlSupport, false);
 });
 
-test('enable ttl using url and options', t => {
+test.serial('enable ttl using url and options', t => {
 	const store = new KeyvEtcd('127.0.0.1:2379', {ttl: 1000});
 	t.deepEqual(store.opts, {
 		url: '127.0.0.1:2379',
@@ -46,7 +46,7 @@ test('enable ttl using url and options', t => {
 	t.is(store.ttlSupport, true);
 });
 
-test('disable ttl using url and options', t => {
+test.serial('disable ttl using url and options', t => {
 	// @ts-expect-error - ttl is not a number, just for test
 	const store = new KeyvEtcd('127.0.0.1:2379', {ttl: true});
 	t.deepEqual(store.opts, {
@@ -70,18 +70,18 @@ test.serial('KeyvEtcd respects default tll option', async t => {
 	t.is(await keyv.get('foo'), null);
 });
 
-test('.delete() with key as number', async t => {
+test.serial('.delete() with key as number', async t => {
 	const store = new KeyvEtcd({uri: etcdUrl});
 	// @ts-expect-error - key needs be a string, just for test
 	t.false(await store.delete(123));
 });
 
-test('.clear() with default namespace', async t => {
+test.serial('.clear() with default namespace', async t => {
 	const store = new KeyvEtcd(etcdUrl);
 	t.is(await store.clear(), undefined);
 });
 
-test('.clear() with namespace', async t => {
+test.serial('.clear() with namespace', async t => {
 	const store = new KeyvEtcd(etcdUrl);
 	store.namespace = 'key1';
 	await store.set(`${store.namespace}:key`, 'bar');
@@ -92,12 +92,43 @@ test('.clear() with namespace', async t => {
 test.serial('close connection successfully', async t => {
 	const keyv = new KeyvEtcd(etcdUrl);
 	t.is(await keyv.get('foo'), null);
-	keyv.disconnect();
+	await keyv.disconnect();
 	try {
 		await keyv.get('foo');
 		t.fail();
 	} catch {
 		t.pass();
 	}
+});
+
+test.serial('iterator with namespace', async t => {
+	const store = new KeyvEtcd(etcdUrl);
+	store.namespace = 'key1';
+	await store.set('key1:foo', 'bar');
+	await store.set('key1:foo2', 'bar2');
+	const iterator = store.iterator('key1');
+	let entry = await iterator.next();
+	// @ts-expect-error - test iterator
+	t.is(entry.value[0], 'key1:foo');
+	// @ts-expect-error - test iterator
+	t.is(entry.value[1], 'bar');
+	entry = await iterator.next();
+	// @ts-expect-error - test iterator
+	t.is(entry.value[0], 'key1:foo2');
+	// @ts-expect-error - test iterator
+	t.is(entry.value[1], 'bar2');
+	entry = await iterator.next();
+	t.is(entry.value, undefined);
+});
+
+test.serial('iterator without namespace', async t => {
+	const store = new KeyvEtcd(etcdUrl);
+	await store.set('foo', 'bar');
+	const iterator = store.iterator();
+	const entry = await iterator.next();
+	// @ts-expect-error - test iterator
+	t.is(entry.value[0], 'foo');
+	// @ts-expect-error - test iterator
+	t.is(entry.value[1], 'bar');
 });
 
