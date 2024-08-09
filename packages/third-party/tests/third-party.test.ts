75e7a8411e063e3139f0558e19b23cc4c8448f1c
Christian Llontop <chrisllontop@icloud.com>
2024-08-08 23:26:41 -0500

    Add additional tests

--- packages/third-party/tests/third-party.test.ts
+++ packages/third-party/tests/third-party.test.ts
@@ -2,21 +2,32 @@ import * as test from 'vitest';
 import Keyv from 'keyv';
 import KeyvFile from 'keyv-file';
 import QuickLRU from 'quick-lru';
-// @ts-expect-error - Keyv Dynamo doesn't have types
-import KeyvDynamo from 'keyv-dynamodb';
 import {keyvApiTests} from '@keyv/test-suite';
+// @ts-expect-error - Keyv LRU doesn't have types
+import {KeyvLru} from 'keyv-lru';
+// @ts-expect-error - Keyv MSSQL doesn't have types
+import KeyvMssql from 'keyv-mssql';
 
+const keyvLruOptions = {
+	max: 1000,
+	notify: false,
+	ttl: 0,
+	expire: 0,
+};
+const keyvLru = () => new KeyvLru(keyvLruOptions);
 const keyvFile = () => new KeyvFile({filename: 'keyv-file.json'});
-const keyvLru = () => new QuickLRU({maxSize: 1000});
-const keyvDynamoDatabase = () => new KeyvDynamo({
-	clientOptions: {
-		region: 'us-east-1',
-		secretAccessKey: 'demo',
-		accessKeyId: 'demo',
+const keyvQuickLru = () => new QuickLRU({maxSize: 1000});
+const keyvMssql = () => new KeyvMssql({
+	connection: {
+		user: 'sa',
+		password: 'YourStrong!Passw0rd',
+		host: 'localhost',
+		database: 'keyv',
 	},
 });
 
-keyvApiTests(test, Keyv, keyvFile as any);
 keyvApiTests(test, Keyv, keyvLru as any);
-keyvApiTests(test, Keyv, keyvDynamoDatabase as any);
-
+keyvApiTests(test, Keyv, keyvFile as any);
+keyvApiTests(test, Keyv, keyvQuickLru as any);
+// KeyvApiTests(test, Keyv, keyvFirestore as any);
+keyvApiTests(test, Keyv, keyvMssql as any);
