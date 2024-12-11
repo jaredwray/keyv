e48dc6f440550ffa6e7ca8d3c0e43bb966305045
Jared Wray <me@jaredwray.com>
2024-12-11 08:21:18 -0800

    updating with emitter

--- packages/keyv/src/index.ts
+++ packages/keyv/src/index.ts
@@ -550,6 +550,7 @@ export class Keyv<GenericValue = any> extends EventManager {
 			result = await store.set(keyPrefixed, serializedValue, ttl);
 		} catch (error) {
 			result = false;
+			this.emit('error', error);
 		}
 
 		this.hooks.trigger(KeyvHooks.POST_SET, {key: keyPrefixed, value: serializedValue, ttl});
