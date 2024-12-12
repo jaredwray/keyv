5edb8c0185cc36b4d1999dd31542a2a0ab69ac31
Hamid Mahdavi <h.mahdavipanah@gmail.com>
2024-12-12 20:45:10 +0300

    redis: add adapter level type generic (#1252)
    * feat: add adapter level type generic
    
    * test: add type tests
    
    ---------
    
    Co-authored-by: Jared Wray <me@jaredwray.com>

---
+++ packages/redis/test/types.test-d.ts
@@ -0,0 +1,42 @@
+import {describe, test, expectTypeOf} from 'vitest';
+import KeyvRedis from '../src/index.js';
+
+describe('KeyvRedis Types', () => {
+	test('should be able to set adapter-level generic value type', async () => {
+    type Value = {foo: string};
+
+    const keyvRedis = new KeyvRedis<Value>();
+
+    expectTypeOf(keyvRedis.get('foo')).toEqualTypeOf<
+    Promise<Value | undefined>
+    >();
+
+    expectTypeOf(keyvRedis.getMany(['foo', 'bar'])).toEqualTypeOf<
+    Promise<Array<Value | undefined>>
+    >();
+
+    expectTypeOf(keyvRedis.iterator()).toEqualTypeOf<
+    AsyncGenerator<[string, Value | undefined], void, unknown>
+    >();
+	});
+
+	test('should be able to set method-level generic value type', async () => {
+    type ValueFoo = {foo: string};
+
+    type ValueBar = {bar: string};
+
+    const keyvRedis = new KeyvRedis<ValueFoo>();
+
+    expectTypeOf(keyvRedis.get<ValueBar>('foo')).toEqualTypeOf<
+    Promise<ValueBar | undefined>
+    >();
+
+    expectTypeOf(keyvRedis.getMany<ValueBar>(['foo', 'bar'])).toEqualTypeOf<
+    Promise<Array<ValueBar | undefined>>
+    >();
+
+    expectTypeOf(keyvRedis.iterator<ValueBar>()).toEqualTypeOf<
+    AsyncGenerator<[string, ValueBar | undefined], void, unknown>
+    >();
+	});
+});
