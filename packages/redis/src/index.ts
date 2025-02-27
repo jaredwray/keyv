3fd2ab15da8f6591d5baabb06f86e77c94100bba
Jared Wray <me@jaredwray.com>
2025-02-27 11:11:33 -0800

    redis - feat: allowing for graceful disconnect via quit and force option (#1321)
    * redis - allowing for graceful disconnect via quit and force option
    
    * Update README.md
    
    * Update index.ts

--- packages/redis/src/index.ts
+++ packages/redis/src/index.ts
@@ -375,10 +375,12 @@ export default class KeyvRedis<T> extends EventEmitter implements KeyvStoreAdapt
 	/**
 	 * Disconnect from the Redis server.
 	 * @returns {Promise<void>}
+	 * @param {boolean} [force] - it will send a quit command if false, otherwise it will send a disconnect command to forcefully disconnect.
+	 * @see {@link https://github.com/redis/node-redis/tree/master/packages/redis#disconnecting}
 	 */
-	public async disconnect(): Promise<void> {
+	public async disconnect(force?: boolean): Promise<void> {
 		if (this._client.isOpen) {
-			await this._client.disconnect();
+			await (force ? this._client.disconnect() : this._client.quit());
 		}
 	}
 
