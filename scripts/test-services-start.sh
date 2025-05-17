fc39f07edb8494c02edfdfeaa76c7dc7216b3781
Jared Wray <me@jaredwray.com>
2025-05-17 10:07:09 -0700

    updating scripts to work correctly

--- scripts/test-services-start.sh
+++ scripts/test-services-start.sh
@@ -5,9 +5,9 @@ architecture=$(uname -m)
 if [[ "$architecture" == "arm"* || "$architecture" == "aarch64" ]]; then
   echo "Running on an ARM platform"
   # Call ARM-specific command
-  docker compose -f ./scripts/docker-compose-arm64.yaml -f ./scripts/docker-compose-redis-cluster.yaml up -d
+  docker compose -f ./scripts/docker-compose-arm64.yaml -f ./scripts/docker-compose-redis-cluster.yaml --project-name keyv up -d
 else
   echo "Running on a non-ARM platform"
   # Call non-ARM command
-  docker compose -f ./scripts/docker-compose.yaml -f ./scripts/docker-compose-redis-cluster.yaml up -d
+  docker compose -f ./scripts/docker-compose.yaml -f ./scripts/docker-compose-redis-cluster.yaml --project-name keyv up -d
 fi
--- scripts/test-services-stop.sh
+++ scripts/test-services-stop.sh
@@ -5,9 +5,9 @@ architecture=$(uname -m)
 if [[ "$architecture" == "arm"* || "$architecture" == "aarch64" ]]; then
   echo "Running on an ARM platform"
   # Call ARM-specific command
-  docker compose -f ./docker-compose-arm64.yaml -f ./docker-compose-redis-cluster.yaml down -v
+  docker compose -f ./scripts/docker-compose-arm64.yaml -f ./scripts/docker-compose-redis-cluster.yaml --project-name keyv down -v
 else
   echo "Running on a non-ARM platform"
   # Call non-ARM command
-  docker compose -f ./docker-compose.yaml -f ./docker-compose-redis-cluster.yaml down -v
+  docker compose -f ./scripts/docker-compose.yaml -f ./scripts/docker-compose-redis-cluster.yaml --project-name keyv down -v
 fi
