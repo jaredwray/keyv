b8c8b1685e8eec88803e942307f9ca4b0cf04147
Jared Wray <me@jaredwray.com>
2025-07-03 15:58:03 -0700

    adding in throwError tests

--- packages/redis/test/properties.test.ts
+++ packages/redis/test/properties.test.ts
@@ -8,13 +8,25 @@ const redisUri = process.env.REDIS_URI ?? 'redis://localhost:6379';
 
 describe('properties', () => {
 	test('should get and set throwOnConnectError', async () => {
-		const keyvRedis = new KeyvRedis(redisUri);
+		const keyvRedis = new KeyvRedis(redisUri, {throwOnConnectError: true});
 		const client = await keyvRedis.getClient();
 		expect(client).toBeDefined();
 
+		expect(keyvRedis.throwOnConnectError).toBe(true);
 		keyvRedis.throwOnConnectError = false;
 		expect(keyvRedis.throwOnConnectError).toBe(false);
 		keyvRedis.throwOnConnectError = true;
 		expect(keyvRedis.throwOnConnectError).toBe(true);
+	});
+
+	test('should get and set throwErrors', async () => {
+		const keyvRedis = new KeyvRedis(redisUri, {throwErrors: true});
+		const client = await keyvRedis.getClient();
+		expect(client).toBeDefined();
+		expect(keyvRedis.throwErrors).toBe(true);
+		keyvRedis.throwErrors = false;
+		expect(keyvRedis.throwErrors).toBe(false);
+		keyvRedis.throwErrors = true;
+		expect(keyvRedis.throwErrors).toBe(true);
 	});
 });
