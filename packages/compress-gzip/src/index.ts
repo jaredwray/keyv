8055cdc48864b77efc58a7d79d2ff4e73bd2c67b
Jared Wray <me@jaredwray.com>
2025-08-16 13:27:20 -0700

    compress-gzip

--- packages/compress-gzip/src/index.ts
+++ packages/compress-gzip/src/index.ts
@@ -1,12 +1,12 @@
-import {inflate, deflate} from 'pako';
-import {defaultSerialize, defaultDeserialize} from '@keyv/serialize';
-import type {Options, Serialize} from './types.js';
+import { defaultDeserialize, defaultSerialize } from "@keyv/serialize";
+import { deflate, inflate } from "pako";
+import type { Options, Serialize } from "./types.js";
 
 export class KeyvGzip {
 	opts: Options;
 	constructor(options?: Options) {
 		this.opts = {
-			to: 'string',
+			to: "string",
 			...options,
 		};
 	}
@@ -17,24 +17,24 @@ export class KeyvGzip {
 
 	async decompress(value: pako.Data, options?: Options) {
 		if (options) {
-			options.to = 'string';
+			options.to = "string";
 		}
 
 		return inflate(value, options || this.opts);
 	}
 
-	async serialize({value, expires}: Serialize) {
-		return defaultSerialize({value: await this.compress(value), expires});
+	async serialize({ value, expires }: Serialize) {
+		return defaultSerialize({ value: await this.compress(value), expires });
 	}
 
 	async deserialize(data: string) {
 		if (data) {
-			const {value, expires}: Serialize = defaultDeserialize(data);
-			return {value: await this.decompress(value as pako.Data), expires};
+			const { value, expires }: Serialize = defaultDeserialize(data);
+			return { value: await this.decompress(value as pako.Data), expires };
 		}
 
 		/* c8 ignore next 1 */
-		return {value: undefined, expires: undefined};
+		return { value: undefined, expires: undefined };
 	}
 }
 
