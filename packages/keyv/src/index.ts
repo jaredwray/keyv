6f1b3aa6d4eb4ed3f2aeabe830bdf7249c3cb714
Jared Wray <me@jaredwray.com>
2025-12-20 09:16:50 -0800

    keyv - fix: updating to return when more specific

--- packages/keyv/src/index.ts
+++ packages/keyv/src/index.ts
@@ -534,7 +534,7 @@ export class Keyv<GenericValue = any> extends EventManager {
 		}
 
 		// If the key is already prefixed, return it
-		if (key.startsWith(this._namespace)) {
+		if (key.startsWith(`${this._namespace}:`)) {
 			return key;
 		}
 
