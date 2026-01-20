e5a71f1e1d761d79dfd68b3d094adb61ee8d1b00
Jared Wray <me@jaredwray.com>
2026-01-20 13:07:12 -0800

    postgres - fix: moving to faker for unique tests (#1814)
    * postgres - fix: moving to faker for unique tests
    
    * test changes

--- packages/postgres/test/test-ssl.ts
+++ packages/postgres/test/test-ssl.ts
@@ -1,3 +1,4 @@
+import { faker } from "@faker-js/faker";
 import * as test from "vitest";
 import KeyvPostgres from "../src/index.js";
 import { endPool } from "../src/pool.js";
@@ -18,7 +19,8 @@ test.it("throws if ssl is not used", async (t) => {
 	await endPool();
 	try {
 		const keyv = new KeyvPostgres({ uri: postgresUri });
-		await keyv.get("foo");
+		const key = faker.string.alphanumeric(10);
+		await keyv.get(key);
 		t.expect.fail();
 	} catch {
 		t.expect(true).toBeTruthy();
@@ -29,21 +31,29 @@ test.it("throws if ssl is not used", async (t) => {
 
 test.it("iterator with default namespace", async (t) => {
 	const keyv = new KeyvPostgres({ uri: postgresUri, ...options });
-	await keyv.set("foo", "bar");
-	await keyv.set("foo1", "bar1");
-	await keyv.set("foo2", "bar2");
-	const iterator = keyv.iterator();
-	let entry = await iterator.next();
-	t.expect(entry.value[0]).toBe("foo");
-	t.expect(entry.value[1]).toBe("bar");
-	entry = await iterator.next();
-	t.expect(entry.value[0]).toBe("foo1");
-	t.expect(entry.value[1]).toBe("bar1");
-	entry = await iterator.next();
-	t.expect(entry.value[0]).toBe("foo2");
-	t.expect(entry.value[1]).toBe("bar2");
-	entry = await iterator.next();
-	t.expect(entry.value).toBeUndefined();
+	const key1 = faker.string.alphanumeric(10);
+	const value1 = faker.lorem.sentence();
+	const key2 = faker.string.alphanumeric(10);
+	const value2 = faker.lorem.sentence();
+	const key3 = faker.string.alphanumeric(10);
+	const value3 = faker.lorem.sentence();
+	await keyv.set(key1, value1);
+	await keyv.set(key2, value2);
+	await keyv.set(key3, value3);
+
+	const keys: string[] = [];
+	const values: string[] = [];
+	for await (const [key, value] of keyv.iterator()) {
+		keys.push(key);
+		values.push(value as string);
+	}
+
+	t.expect(keys).toContain(key1);
+	t.expect(keys).toContain(key2);
+	t.expect(keys).toContain(key3);
+	t.expect(values).toContain(value1);
+	t.expect(values).toContain(value2);
+	t.expect(values).toContain(value3);
 });
 
 test.it(".clear() with undefined namespace", async (t) => {
@@ -53,10 +63,11 @@ test.it(".clear() with undefined namespace", async (t) => {
 
 test.it("close connection successfully", async (t) => {
 	const keyv = store();
-	t.expect(await keyv.get("foo")).toBeUndefined();
+	const key = faker.string.alphanumeric(10);
+	t.expect(await keyv.get(key)).toBeUndefined();
 	await keyv.disconnect();
 	try {
-		await keyv.get("foo");
+		await keyv.get(key);
 		t.expect.fail();
 	} catch {
 		t.expect(true).toBeTruthy();
