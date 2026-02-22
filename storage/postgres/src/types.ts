35d3058edcf42ae6839e6f13d631093cf044692d
Jared Wray <me@jaredwray.com>
2026-02-21 17:47:08 -1000

    postgres - feat: moving to properties

--- storage/postgres/src/types.ts
+++ storage/postgres/src/types.ts
@@ -1,26 +1,5 @@
 // biome-ignore-all lint/suspicious/noExplicitAny: type format
-import type { StoredData } from "keyv";
 import type { PoolConfig } from "pg";
-
-export type IteratorOutput = AsyncGenerator<any, void, any>;
-
-export type GetOutput<Value> = Promise<Value | undefined>;
-
-export type GetManyOutput<Value> = Promise<
-	Array<StoredData<Value | undefined>>
->;
-
-export type SetOutput = Promise<any>;
-
-export type DeleteOutput = Promise<boolean>;
-
-export type DeleteManyOutput = Promise<boolean>;
-
-export type ClearOutput = Promise<void>;
-
-export type HasOutput = Promise<boolean>;
-
-export type DisconnectOutput = Promise<void>;
 
 export type KeyvPostgresOptions = {
 	uri?: string;
