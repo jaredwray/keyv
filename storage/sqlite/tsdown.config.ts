3fc6d8adba0ea406272fd798a16e4b90f1b7349b
Jared Wray <me@jaredwray.com>
2026-03-31 12:43:01 -0700

    mono - feat: moving to tsdown for build (#1926)
    * mono - feat: moving to tsdown for build
    
    * moving to base config
    
    * moving to base config
    
    * base fix
    
    * moving to ts base

---
+++ storage/sqlite/tsdown.config.ts
@@ -0,0 +1,10 @@
+// @ts-ignore - tsdown requires .ts extension for config imports
+import base from '../../tsdown.base.ts';
+
+export default {
+	...base,
+	entry: ['src/index.ts'],
+	deps: {
+		neverBundle: ['bun:sqlite', 'node:sqlite'],
+	},
+};
